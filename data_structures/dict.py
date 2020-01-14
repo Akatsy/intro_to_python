c17120b4f51a3a57a318f74a5ae5688b8e1bd293
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-14 12:00:40 +0300

    store mappings of a collection of key-value pairs using a dict

---
+++ data_structures/dict.py
@@ -0,0 +1,10 @@
+print("Dictionaries are a data type for a mutable unordered mapping of unique keys to values")
+print("The key has to be unique because it is used to access the value to print it out, or modify it")
+print("The key has to be of an immutable data type like int, float, string, tuple and cannot be of a mutable data type like sets or lists")
+print('#' * 79)
+print("The dict can be created as follows:")
+thisdict = {'brand': 'Ford','model': 'Mustang','year': 1964}
+print("By using curly braces '{}' to sorround a comma separated collection of key-value pairs delimited by a colon (:) as follows thisdict = {'brand': 'Ford','model': 'Mustang','year': 1964} and printing it out, you get\n", thisdict)
+print('#' * 79)
+n = {}
+print("When you declare a variable and assign it an empty set of curly braces, it is assigned the type dict e.g n = {} and type(n) will give; ", type(n))
