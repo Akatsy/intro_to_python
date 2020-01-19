f0f1af12b956fa01ed2221814b16ad6011587dcf
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-19 13:59:55 +0300

    solve a quiz with for loops

--- loops/for_loop.py
+++ loops/for_loop.py
@@ -110,3 +110,16 @@ print("There are {} xml tags in the list of strings, tokens".format(count))
 
 print('#' * 79)
 
+# Write some code, including a for loop, that iterates over a list of strings and creates a single string, html_str, which is an HTML list. For example, if the list is items = ['first string', 'second string'], printing html_str should output:
+
+# <ul>
+# <li>first string</li>
+# <li>second string</li>
+# </ul>
+items = ['first string', 'second string']
+html_str = '<ul>\n'
+for item in items:
+    item = '<li>' + item + '</li>' + '\n'
+    html_str += item
+html_str += '</ul>'
+print(html_str)
