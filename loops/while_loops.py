fcc8f89d64f412635554fb0ddabef42406c84cbe
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-21 07:27:19 +0300

    count using while loops

--- loops/while_loops.py
+++ loops/while_loops.py
@@ -17,3 +17,38 @@ while current <= number:
     factorial *= current
     current += 1
 print(factorial)
+
+print('#' * 79)
+# Suppose you want to count from some number start_num by another number count_by until you hit a final number end_num. Use break_num as the variable that you'll change each time through the loop. For simplicity, assume that end_num is always larger than start_num and count_by is always positive.
+start_num = 5 #provide some start number
+end_num = 100 #provide some end number that you stop when you hit
+count_by = 2 #provide some number to count by 
+
+# write a while loop that uses break_num as the ongoing number to 
+#   check against end_num
+break_num = start_num
+while break_num < end_num:
+    print(break_num)
+    break_num += count_by
+print(break_num)
+
+print('#' * 79)
+# Suppose you want to count from some number start_num by another number count_by until you hit a final number end_num, and calculate break_num the way you did in the last quiz.
+
+# Now in addition, address what would happen if someone gives a start_num that is greater than end_num. If this is the case, set result to "Oops! Looks like your start value is greater than the end value. Please try again." Otherwise, set result to the value of break_num.
+start_num = 10 #provide some start number
+end_num = 9 #provide some end number that you stop when you hit
+count_by = 1 #provide some number to count by 
+
+# write a condition to check that end_num is larger than start_num before looping
+# write a while loop that uses break_num as the ongoing number to 
+#   check against end_num
+break_num = start_num
+if end_num > start_num:
+    result = break_num
+else:
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+while break_num < end_num:
+    print(break_num)
+    break_num += count_by
+print(result)
