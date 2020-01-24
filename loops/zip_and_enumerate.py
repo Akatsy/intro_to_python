b90f4275a73e2f260a44283daef3bfe4e8a49520
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-24 10:53:46 +0300

    use zip in a for loop to create a string

--- loops/zip_and_enumerate.py
+++ loops/zip_and_enumerate.py
@@ -34,3 +34,23 @@ for i, letter in enumerate(teams):
     print(i, letter)
 
 print("You will often use enumerate() when you need the indices and values of a list in a loop")
+
+print('#' * 99)
+# quiz
+# Use zip to write a for loop that creates a string specifying the label and coordinates of each point and appends it to the list points. Each string should be formatted as label: x, y, z. For example, the string for the first coordinate should be F: 23, 677, 4
+
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+# write your for loop here
+for object in zip(labels,x_coord,y_coord,z_coord):
+    object = object[0] + ': ' + str(object[1]) + ', ' + str(object[2]) + ', ' + str(object[3])
+    points.append(object)
+    
+
+
+for point in points:
+    print(point)
