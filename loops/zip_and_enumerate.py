bba095ce8545d10f6269645f08a533338e2996c3
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-24 11:29:24 +0300

    unzip tuples

--- loops/zip_and_enumerate.py
+++ loops/zip_and_enumerate.py
@@ -66,3 +66,16 @@ cast_heights = [72, 68, 72, 66, 76]
 
 cast = dict(zip(cast_names, cast_heights))
 print(cast)
+
+print('#' * 99)
+# Quiz: Unzip Tuples
+# Unzip the cast tuple into two names and heights tuples.
+
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names, heights = zip(*cast)
+
+print(names)
+print(heights)
+
