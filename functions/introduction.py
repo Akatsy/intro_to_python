fb5ea7a5a8765f088a2f378a2f3eeec7181b9b5e
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-28 17:05:07 +0300

    functions without arguments plus the return keyword

--- functions/introduction.py
+++ functions/introduction.py
@@ -8,6 +8,11 @@ def volume_cylinder(height, radius):
     volume = pi * radius **2 * height
     return volume
 print('#' * 99)
+
+print("Then once you have declared a function, you can call it (use it) by calling the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
+print(volume_cylinder(45, 450))
+
+print('#' * 99)
 print("Defining a function is as easy as above. It has a few parts as follows:")
 print("1. 'def' keyword- A function definition always begins with the def keyword. This tells Python that whatever follows is a function definition. That statement that starts with def keyword must always end with a colon (:)")
 print('#' * 99)
@@ -18,7 +23,19 @@ print("3. After the function name is a parantheses that houses any number of arg
 
 print('#' * 99)
 print("4. After the definition statement that ends with a colon, comes the body of the function which must be indented below it. The body can contain other variable declarations and any number of code blocks to perform a task")
+print('#' * 99)
+print("An example of a function without arguments\ndef greetings():\n\tprint(\"Hello! hey! alloha!\")\n")
+
+def greetings():
+    print("Hello! hey! alloha!")
 
 print('#' * 99)
-print("Then once you have declared a function, you can call it (use it) by calling the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
-print(volume_cylinder(45, 450))
+print("calling the function as follows\nprint(greetings()) we get the output:")
+print(greetings())
+
+print('#' * 99)
+print("We get back the code block inside the function body and the None keyword. Why is the None keyword given as an output?")
+
+print('#' * 99)
+print("Often, the body of a Python function will have the return keyword followed by an expression whose value is what return will give as the output of the function. So the return keyword is used by  a function to give output from the function")
+print("If the return keyword is not used in a function body explicitly to define the output, then the function returns None keyword as seen from the example above")
