5266717adfacd5a8d05fde404776065562982237
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-29 15:48:19 +0300

    default arguments, positional parameters, named parameters

--- functions/introduction.py
+++ functions/introduction.py
@@ -43,4 +43,18 @@ print("Often, the body of a Python function will have the return keyword followe
 print("If the return keyword is not used in a function body explicitly to define the output, then the function returns None keyword as seen from the example above")
 
 print('#' * 99)
-print("")
+print("When defining a function, you can provide default arguments.\nThis means giving a value to the argument which will be used as the default value if when calling a function the user does not specify a value for that argument")
+print('#' * 99)
+
+print("For example in our cylinder volume function, we can include a radius default argument as follows:")
+print("def volume_cylinder(height, radius=7):\n\tpi = 3.14159265359\n\treturn pi * radius **2 * height")
+print('#' * 99)
+print("Default arguments are useful when we want to be more conscise with our code in scenarios where there is a commonly used value for an argument but we still want it to be customizable.")
+print("If the user when calling the function, supplies a value for the default argument parameter, then the user's argument will override the default argument")
+
+print('#' * 99)
+print("There are two ways to supply arguments to a function:")
+print("1. positional arguments- This is where we give values inside the parantheses that match the order of the defined arguments i.e if the first argument was height and second was radius then we specify them in that same order when calling the function e.g cylinder_volume(10, 6) so the height will be 10 and radius will be 6")
+print('#' * 99)
+
+print("2. named arguments- This is where we give values inside the parantheses using their argument names and then supply a value e.g cylinder_volume(radius=10, height=6) so the height will be 6 and radius will be 10")
