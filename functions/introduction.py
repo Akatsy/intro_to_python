fd797a4b00987bd6324f761ffec915de6e135e13
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-29 15:25:05 +0300

    better wording

--- functions/introduction.py
+++ functions/introduction.py
@@ -9,20 +9,22 @@ def volume_cylinder(height, radius):
     return volume
 print('#' * 99)
 
-print("Then once you have declared a function, you can call it (use it) by calling the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
+print("Then once you have declared a function, you can call it (use it) by using the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
 print(volume_cylinder(45, 450))
 
 print('#' * 99)
-print("Defining a function is as easy as above. It has a few parts as follows:")
+print("Defining a function is as easy as above. It has a few important parts as follows:")
+print("A. Function header - This is the first statement in a function definition. It consists of;")
 print("1. 'def' keyword- A function definition always begins with the def keyword. This tells Python that whatever follows is a function definition. That statement that starts with def keyword must always end with a colon (:)")
 print('#' * 99)
-print("2. function name- Immediately after the def keyword, what follows is the name of the function. This is the command we will call to run the code encapsulated in it. The rules for naming a function are similar to those of naming Python variables")
+print("2. function name- After the def keyword, what follows is the name of the function. This is the command we will call to run the code encapsulated in it. The rules for naming a function are similar to those of naming Python variables.")
 
 print('#' * 99)
-print("3. After the function name is a parantheses that houses any number of arguments for the function. Arguments are variables that the function will use in it's body to generate output. Arguments are not mandatory and a function can have none. In this case, the parantheses is left blank")
+print("3. Immediately after the function name is a parantheses that houses any number of arguments for the function. Arguments are variables that the function will use in it's body to generate output. They are the inputs that the function will accept when called. Arguments are not mandatory and a function can have none. In this case, the parantheses is left blank")
 
 print('#' * 99)
-print("4. After the definition statement that ends with a colon, comes the body of the function which must be indented below it. The body can contain other variable declarations and any number of code blocks to perform a task")
+print("B. Function body - This is the indented block of code below the function header")
+print("4. After the definition statement that ends with a colon, comes the body of the function which must be indented below it. It is where the function does it's work. The body can contain other variable declarations and any number of statements or code blocks to perform a task")
 print('#' * 99)
 print("An example of a function without arguments\ndef greetings():\n\tprint(\"Hello! hey! alloha!\")\n")
 
@@ -37,5 +39,8 @@ print('#' * 99)
 print("We get back the code block inside the function body and the None keyword. Why is the None keyword given as an output?")
 
 print('#' * 99)
-print("Often, the body of a Python function will have the return keyword followed by an expression whose value is what return will give as the output of the function. So the return keyword is used by  a function to give output from the function")
+print("Often, the body of a Python function will have the return keyword followed by an expression whose value is what return will give as the output value of the function. So the return keyword is used by  a function to give an output value from the function")
 print("If the return keyword is not used in a function body explicitly to define the output, then the function returns None keyword as seen from the example above")
+
+print('#' * 99)
+print("")
