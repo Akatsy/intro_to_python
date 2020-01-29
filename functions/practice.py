bfd94647753c0213138e1afe54dc45a59775c21f
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-29 16:40:34 +0300

    function to return number of weeks and days given days

--- functions/practice.py
+++ functions/practice.py
@@ -15,3 +15,23 @@ print("expected result: {}, actual result: {}".format(expected_result1, test1))
 test2 = population_density(864816, 121.4)
 expected_result2 = 7123.6902801
 print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+
+print('#' * 99)
+
+# Quiz: readable_timedelta
+# Write a function named readable_timedelta. The function should take one argument, an integer days, and return a string that says how many weeks and days that is. For example, calling the function and printing the result like this:
+
+# print(readable_timedelta(10))
+# should output the following:
+
+# 1 week(s) and 3 day(s).
+
+def readable_timedelta(days):
+    wks = days // 7
+    num_days = days % 7
+    return "{} week (s) and {} day (s).".format(wks, num_days)
+print(readable_timedelta(15))
+print(readable_timedelta(100))
+print(readable_timedelta(366))
+print(readable_timedelta(365))
