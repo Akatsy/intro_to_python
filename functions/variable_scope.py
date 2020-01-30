4f15aec133d25413d46bcefeb75d2bcdb631f3ea
mutwiri-2 <devmutwiri2@gmail.com>
2020-01-30 17:11:08 +0300

    global variable scope, correct bug in random string generator

--- functions/variable_scope.py
+++ functions/variable_scope.py
@@ -2,7 +2,9 @@ print("Variable scope is which part of a program a variable can be referenced (u
 print("There are two types of variable scope:\n\t1. local scope\n\t2.global scope")
 print("Variables defined within a function are said to have a scope that is local meaning that the variable can only be accessed and used within that function. Using or accessing a variable with a local scope outside the function it is defined in is not possible")
 print('#' * 99)
-print("eg of a local variable")
+print("eg of a local variable count used to get the number of occurences of a substring in a string")
+print("def word_count(string, search_term):\n\tcount = 0\n\tfor letter in string:\n\t\tcount += 1\n\treturn count")
+print("Calling the function as follows: print(word_count(random_string, 'a')) after defining a random_string: random_string = \"VGJYJJjEIRCNFWuEVBHiYzVJsmMYlWMlHQrarfCcWzprOHEjksmzvlHlFwnxQqmzurrBkpasHIWqQawKKcBYszvsyXzOLyogy\" would give us:")
 random_string = "VGJYJJjEIRCNFWuEVBHiYzVJsmMYlWMlHQrarfCcWzprOHEjksmzvlHlFwnxQqmzurrBkpasHIWqQawKKcBYszvsyXzOLyogy"
 def word_count(string, search_term):
     count = 0
@@ -14,15 +16,18 @@ def word_count(string, search_term):
 print(word_count(random_string, 'a'))
 
 print('#' * 99)
+print("A variable defined outside any function is said to have a global scope. It is accessible inside any function within the program but it's value can only be accessed and cannot be changed")
+print('#' * 99)
 
+# generate a random string using alphanumeric characters 
 def random_str_generator(len_str):
     import random
     import string
     random_str = ""
     alpha_numeric_chars = list(string.ascii_letters) + list(string.digits)
-    while len(random_str) <= len_str:
+    while len(random_str) < len_str:
         random_str += random.choice(alpha_numeric_chars)
     return random_str
     
-print(random_str_generator(1000))
+print(random_str_generator(10))
 
