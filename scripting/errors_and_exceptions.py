1a4c2df13f336e52ab098ad1a99b26e1757bad1f
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-06 12:21:40 +0300

    some built-in python exceptions

--- scripting/errors_and_exceptions.py
+++ scripting/errors_and_exceptions.py
@@ -13,6 +13,15 @@ print("1. try: This is the only mandatory clause in the try statement and its co
 print("2. except: If Python runs into any exception while running code in the try block, it jumps to the except block that handles the exception")
 print("3. else: If Python runs into no exceptions while running the code in the try block, it will execute the code in else after executing the try block")
 print("4. finally: This block will always be executed under any circumstances before the try statement exists even if it is ending the program. The finally block is useful for cleaning up actions in our code e.g closing a file we attempted opening using a try statement")
+print('#' * 99)
+
+print("There are many built-in exceptions in Python and when they occur, Python's error messages point out wha t exception was catched before the program crashed e.g")
+print("1. ValueError: An object of the correct type but inappropriate value is passed as input to a built-in function or operation")
+print("2. TypeError: An object of an unsupported type is passed to a function or operation")
+print("3. IndexError: An exception that occurs when a sequence subscript is out of range")
+print("4. AssertionError: An exception that occurs when an assert statement fails")
+print("1. KeyError: An exception that occurs when a key does not exist in a dictionary")
+print('#' * 99)
 
 while True:
     try:
