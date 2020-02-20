f74d9ca18a84b2ab0eb5aee48147dbb5d269473e
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-20 18:03:10 +0300

    a number guessing game

--- control_flow/guess_a_number.py
+++ control_flow/guess_a_number.py
@@ -1,9 +1,12 @@
-answer = 56   #the number you want guessed
-guess = 34    # the player's guess
-
-if answer > guess:
-    print("Try a higher value")
-elif answer < guess:
-    print("Try a lower value")
-elif answer == guess:
-    print("Correct! That's the number!!")
+answer = int(input('Please input the number you want guessed: '))   #the number you want guessed
+while True:
+    guess = int(input('Please input your guess: '))   # the player's guess
+    if answer > guess:
+        print("Try a higher value")
+        continue
+    elif answer < guess:
+        print("Try a lower value")
+        continue
+    elif answer == guess:
+        print("Correct! That's the number!!")
+        break
