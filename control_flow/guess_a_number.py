5e5318bc4e47c94799ffe516e3420a50f88bfa68
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-20 18:07:24 +0300

    more succint number guess game

--- control_flow/guess_a_number.py
+++ control_flow/guess_a_number.py
@@ -3,10 +3,8 @@ while True:
     guess = int(input('Please input your guess: '))   # the player's guess
     if answer > guess:
         print("Try a higher value")
-        continue
     elif answer < guess:
         print("Try a lower value")
-        continue
     elif answer == guess:
         print("Correct! That's the number!!")
         break
