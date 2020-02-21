c18b44d7ce82dc14f1ae1cab54e3e417eed680ca
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-21 21:04:28 +0300

    zip iterables of different length

--- rough_work/get_dirty.py
+++ rough_work/get_dirty.py
@@ -13,3 +13,8 @@ my_list[3:6] = [4,5,6]
 print(my_list)
 
 print('#' * 99)
+
+# what happens when you use zip on iterables of different length?
+print(list(zip(['a','b','c','d'],[1,2,3])))
+for letter, number in zip(['a','b','c','d'],[1,2,3]):
+    print(letter, number)
