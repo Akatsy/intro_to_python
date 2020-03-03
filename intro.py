459550009d42f8fd73a151a5f74e6a6d4fec2b4e
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-03 12:25:23 +0300

    definition of Python, zen of Python

---
+++ intro.py
@@ -0,0 +1,6 @@
+print("Python is an interpreted, high-level general-purpose programming language developed by Guido van Rossum and first released in 1991")
+print("Python's design philosophy emphasizes code readability with its notable use of significant whitespaces")
+print("The language's core philosophy is summarized in the document 'The Zen of Python'")
+print("The Zen of Python is a collection of '19 guiding principles' for writing computer programs that influence the design of the Python programming language")
+print("It is included as an easter egg in the Python interpreter which can be displayed by entering\nimport this\n")
+import this
