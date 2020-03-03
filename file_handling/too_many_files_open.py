192e9479d235ef75c299ebcaa4cb5439fcabbf8a
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-03 15:41:18 +0300

    use try block to catch too many files open exception

--- file_handling/too_many_files_open.py
+++ file_handling/too_many_files_open.py
@@ -1,4 +1,11 @@
 files = []
-for i in range(10000):
-    files.append(open('/home/mutwiri2/Desktop/intro_to_python/file_handling/if'))
-    print(i)
+# for i in range(10000):
+#     files.append(open('/home/mutwiri2/Desktop/intro_to_python/file_handling/if'))
+#     print(i)
+
+try:
+    for i in range(10000):
+        files.append(open('/home/mutwiri2/Desktop/intro_to_python/file_handling/if'))
+        print(i)
+except Exception as e:
+    print(e)
