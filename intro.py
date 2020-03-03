2200a9b7c6daa9c06506d8b553123124f1999e99
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-03 14:11:54 +0300

    points to note

--- intro.py
+++ intro.py
@@ -9,3 +9,8 @@ import this
 
 print('#' * 99)
 print("Python is a powerful general-purpose programming language with applications ranging form web and internet development, scientific and numeric (mathematics, science, and engineering, data analysis and modelling, deep learning, machine learning, AI, visualization etc), creating Desktop GUIs, Software and game development, Education, scripting, Network Programming among others ")
+print('#' * 99)
+print("A few points to note before learning Python:")
+print("Python is case-sensitive so 'name' and 'Name' are not the same")
+print("Spacing matters in Python - Python is not heavy on syntax so it relies heavily on spacing to delimit code blocks")
+print("You will inevitably run into errors as you learn Python. Errors do not make you a bad programmer, it is just a way for the computer to tell you that it does not quite understand what you have instructed it to do. Use errors as a learning tool, don't let them be a deterrent or discouragement in your learning to code journey")
