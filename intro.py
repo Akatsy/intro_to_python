078de3f4cdb91ea684effd24e42856f7fe68d8ce
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-03 13:58:39 +0300

    applications of python

--- intro.py
+++ intro.py
@@ -1,6 +1,11 @@
 print("Python is an interpreted, high-level general-purpose programming language developed by Guido van Rossum and first released in 1991")
+print('#' * 99)
 print("Python's design philosophy emphasizes code readability with its notable use of significant whitespaces")
 print("The language's core philosophy is summarized in the document 'The Zen of Python'")
+print('#' * 99)
 print("The Zen of Python is a collection of '19 guiding principles' for writing computer programs that influence the design of the Python programming language")
 print("It is included as an easter egg in the Python interpreter which can be displayed by entering\nimport this\n")
 import this
+
+print('#' * 99)
+print("Python is a powerful general-purpose programming language with applications ranging form web and internet development, scientific and numeric (mathematics, science, and engineering, data analysis and modelling, deep learning, machine learning, AI, visualization etc), creating Desktop GUIs, Software and game development, Education, scripting, Network Programming among others ")
