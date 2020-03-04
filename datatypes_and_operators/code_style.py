9e650c5c3a70ad3a64ae5fe5d18cf5ed11cc1e75
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-04 12:38:33 +0300

    code style guide for Python

---
+++ datatypes_and_operators/code_style.py
@@ -0,0 +1,11 @@
+# best practices for code style in Python
+print("When you call a function, put the opening parantheses straight after the name of the function e.g print(8) and not print (8)")
+print('#' * 99)
+print("Do not put any extra spaces immediately after the parantheses either e.g print( 8*5 ) is bad style. Rather print(8*5) is better")
+print('#' * 99)
+print("If you are mixing operators with different priorities, add a space around the lower priority(the one that comes later in the arithmetic order of operations) to make the code easier to read e.g")
+print("5*7 + 4")
+print('#' * 99)
+print("Do not write extremely long lines of code. Limit your lines to 79-99 characters long")
+print('#' * 79)
+print("For more style guidelines, read the official style guide-  PEP 8 -- Style Guide for Python Code at https://www.python.org/dev/peps/pep-0008/")
