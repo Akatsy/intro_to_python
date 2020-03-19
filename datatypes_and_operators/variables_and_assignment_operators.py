c9513d178bb39529cf00e4f0b68c62dc569bbf8e
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-19 18:57:13 +0300

    better wording, correct typos

--- datatypes_and_operators/variables_and_assignment_operators.py
+++ datatypes_and_operators/variables_and_assignment_operators.py
@@ -1,16 +1,18 @@
 #Variables and assignment operators - how to store data in Python
 print("Variables are a way to store data so as to access that data later. The power this brings to programming is immense")
 print('#' * 79)
-print("This is how to declare a variable to store data(value of any type e.g a number):")
+print("This is how to declare a variable to store data (value of any type e.g a number):")
 print("variable_name = value")
 print('#' * 79)
-print("Whatever term is on the left(variable_name) becomes a name for whatever value is on the right. The '=' sign is what is called 'assignment operator and all it does is assigns value on the right to the name on the left. N.B: THE '=' SIGN IS NOT FOR EQUALITY!! IT DOES NOT MEAN THE NAME ON THE LEFT IS EQUAL TO THE VALUE ON THE RIGHT AND THE VALUE ON THE RIGHT IS EQUAL TO THE NAME ON THE LEFT AS THE CONVENTIONAL '=' SIGN MEANS IN MATH.")
+print("Whatever term is on the left (variable_name) becomes a name for whatever value is on the right. The '=' sign is what is called 'assignment operator and all it does is it assigns value on the right to the variable_name on the left.")
+print("N.B: THE '=' SIGN DOES NOT SIGNIFY EQUALITY!! IT DOES NOT MEAN THAT THE NAME ON THE LEFT IS EQUAL TO THE VALUE ON THE RIGHT AND CONVERSELY THAT THE VALUE ON THE RIGHT IS EQUAL TO THE NAME ON THE LEFT AS THE CONVENTIONAL '=' SIGN MEANS IN MATH.")
 print('#' * 79)
 print("'top_student_marks = 450' assigns the value 450 to the variable top_student_marks which is self eplanatory i.e the top student had 450 marks. Once you have assigned a value to a variable, you can access that value using the variable name as shown below:")
+print("print(Top student in the exam got, top_student_marks)")
 top_student_marks = 450
 print("Top student in the exam got", top_student_marks)
 print('#' * 79)
-print("As seen above, variable names should be representative(descriptive) of the values they hold. This is good practice and helps you avoid common pitfalls when naming variables")
+print("As seen above, variable names should be representative (descriptive) of the values they hold. This is good practice and helps you avoid common pitfalls when naming variables")
 print("Variable names should follow the following rules:")
 print("\t1. can only contain ordinary letters, numbers and underscores - so no spaces or special characters allowed")
 print("\t2. must begin with a letter or underscore, so cannot begin with a number")
@@ -31,14 +33,15 @@ print("2. add the weight gained to the variable and then reassign it like 'weigh
 weight = 56
 print("my weight is", weight)
 weight = 58
-print("my new weight after re-assigning is", weight)
+print("weight = 58\nmy new weight after re-assigning is", weight)
+weight = 56
 weight = weight + 2
-print("my new weight after adding weight gained is", weight)
+print("weight = weight + 2\nmy new weight after adding weight gained is", weight)
 print('#' * 79)
 print("There are special assignment operators in python to deal with the above common use-case of performing an arithmetic operation to a variable e.g adding and then re-assigning the new value to the variable")
-print("The special assignment operators are all the arithmetic operators followed by the '='(assignment operator) e.g the two most common '+=' and '-='. What they do is just apply the arithmetic operation to the variable on the left using the value on the right and then assign it back to the variable")
+print("The special assignment operators are all the arithmetic operators followed by the '='(assignment operator) e.g the two most common '+=' and '-='. What they do is just apply the arithmetic operation to the variable on the left using the value on the right and then assign the result back to the variable")
 print('#' * 79)
-print("e.g, say you have 'x=2', and you want to increase x by 2 then decrease x by 1 then multiply x by 3 then raise x to the power of 4 then do float division of x by 6 and finally integer division of x by 5 you would do the following:")
+print("e.g, say you have 'x=2', and you want to increase x by 2 then decrease x by 1 then multiply x by 3 then raise x to the power of 4 then do integer division of x by 6 and finally float division of x by 5 you would do the following:")
 x = 2
 print('x = 2 gives', x)
 x += 2
@@ -49,16 +52,17 @@ x *= 3
 print('x *= 3 gives', x)
 x **= 4
 print('x **= 4 gives', x)
-x /= 6
-print('x /= 6 gives', x)
-x //= 5
-print('x //= 5 gives', x)
+x //= 6
+print('x //= 6 gives', x)
+x /= 5
+print('x /= 5 gives', x)
 print('#' * 79)
-print('Below are python keywords that cannot(read as should not- because really, you can use them but they will lead to unwanted errors eventually when you try using them for their reserved purposes in Python) be used as variable names is:')
+print('Below are python keywords that cannot be used as variable names (read as \'should not\'- because really you can use them but they will lead to unwanted errors eventually when you try using them for their reserved purposes in Python):')
+print("")
 import keyword
 for index, keyword in enumerate(keyword.kwlist):
     print(index, keyword)
-# print(keyword.kwlist)
+# print(keyword.kwlist) - prints out a list of Python keywords
 
 print('#' * 79)
 print('You can check if a word is a python keyword by typing the following on your REPL(A read–eval–print loop, also termed an interactive toplevel or language shell, is a simple, interactive computer programming environment that takes single user inputs (i.e., single expressions), evaluates (executes) them, and returns the result to the user;) and replacing "variable_name" with the name you want to check:')
@@ -68,10 +72,11 @@ print('#' * 79)
 print("As a programmer, you cannot avoid bugs or errors in your code. Errors do not make you a bad programmer-- they are just a way of the computer telling you that it does not understand what you have instructed it to do. Do not fear them, instead use them to learn to write better programs that the computer understands and executes to give the desired output")
 print('#' * 79)
 print("Let's generate our first error by trying to access a variable name we have not defined:")
-print("The statement below 'print(my_variable)' generates an error called 'NameError' and as it states 'my_variable is not defined")
+print("The statement 'print(my_variable)' below generates an error called 'NameError' and as it states 'my_variable' is not defined")
 print('#' * 79)
 print("An error like the one below(syntax or parsing error) forces the program to stop execution at the point of failure and exit thus any statements after are not executed")
 print("Note: Python Interpreter reads and executes python code from top to bottom")
 print('#' * 79)
+print("print(my_variable)")
 print(my_variable)
 print('Hello there!') #will not be executed because the line preceding it has a syntactic or parsing error
