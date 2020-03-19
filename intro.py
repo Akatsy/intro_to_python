f2119d21668aa78a833cf53396142d35e3f1072b
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-19 17:46:52 +0300

    better wording

--- intro.py
+++ intro.py
@@ -16,7 +16,7 @@ print("Python's design philosophy emphasizes code readability with its notable u
 print("The language's core philosophy is summarized in the document 'The Zen of Python'")
 print('#' * 99)
 print("The Zen of Python is a collection of '19 guiding principles' for writing computer programs that influence the design of the Python programming language")
-print("It is included as an easter egg in the Python interpreter which can be displayed by entering\nimport this\n")
+print("It is included as an easter egg in the Python interpreter which can be displayed by typing the following statement on the Python interpreter:\nimport this\n")
 import this
 
 print('#' * 99)
