4ca6a5bc62ae8afba6a943a5334c477ee5f03fc0
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-22 15:14:47 +0300

    fix typo

--- datatypes_and_operators/dir_and_help_functions.py
+++ datatypes_and_operators/dir_and_help_functions.py
@@ -1,4 +1,4 @@
-print("The dir() built-in fuction returns information about an object. It can be useful when you have an object and want to quickly find out what you can do with it. It returns some of the attributes and methods of that object(Remember every object you encounter in Python has a type and this is what will determine the attributes and methods returned)")
+print("The dir() built-in fuction returns information about an object. It can be useful when you have an object and want to quickly find out what you can do with it. It returns some of the attributes and methods of that object (Remember every object you encounter in Python has a type and this is what will determine the attributes and methods returned)")
 print('#' * 79)
 num = 5
 print("Say you have an integer like 5 assigned to the variable num, calling dir() on num will result in the following attributes")
@@ -12,3 +12,4 @@ print('#' * 79)
 
 print("The information printed is not very useful so we have another builtin function called help() which gives a short description of any of the attributes returned by dir(). You call help() on the attribute using the dot notation e.g help(num.__add__)")
 print('#' * 79)
+# print(help(num.__add__))
