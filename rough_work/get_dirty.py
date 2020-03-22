ee9a45470a1feab12d0a29ef9ede06b5c6098869
mutwiri-2 <devmutwiri2@gmail.com>
2020-03-22 14:03:48 +0300

    len fuction on tab, newline and space characters

--- rough_work/get_dirty.py
+++ rough_work/get_dirty.py
@@ -1,11 +1,23 @@
 tab = '    '
-print(len(tab))
+print("If you declare a variable tab and assign to it one tab from the keyboard then len(tab) returns",len(tab))
+print('#'*79)
+
+tab_2 = '       '
+print("If you declare a variable tab_2 and assign to it two tabs from the keyboard then len(tab_2) returns",len(tab_2))
+print('#'*79)
+
+tab_3 = '           '
+print("If you declare a variable tab_3 and assign to it three tabs from the keyboard then len(tab_3) returns",len(tab_3))
+print('#'*79)
+
 newline = '''
 '''
-print(len(newline))
-print(len('\t'))
-print(len('\n'))
-print(len(' '))
+print("If you declare a variable newline and assign to it a multiline string like below\nnewline = '''\n'''\n then len(newline) will return ",len(newline))
+print('#'*79)
+
+print("len('\\t') returns",len('\t'))
+print("len('\\n') returns",len('\n'))
+print("len(' ') returns",len(' '))
 print('#' * 99)
 
 my_list = [1,2,3]
