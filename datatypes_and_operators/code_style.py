39d6f757d95358fa851ab71f088c4f44a4b92761
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-01 14:29:42 +0300

    fix typo

--- datatypes_and_operators/code_style.py
+++ datatypes_and_operators/code_style.py
@@ -3,7 +3,7 @@ print("When you call a function, put the opening parantheses straight after the 
 print('#' * 99)
 print("Do not put any extra spaces immediately after the parantheses either e.g print( 8*5 ) is bad style. Rather print(8*5) is better")
 print('#' * 99)
-print("If you are mixing operators with different priorities, add a space around the lower priority(the one that comes later in the arithmetic order of operations) to make the code easier to read e.g")
+print("If you are mixing operators with different priorities, add a space around the lower priority (the one that comes later in the arithmetic order of operations) to make the code easier to read e.g")
 print("5*7 + 4")
 print('#' * 99)
 print("Do not write extremely long lines of code. Limit your lines to 79-99 characters long")
