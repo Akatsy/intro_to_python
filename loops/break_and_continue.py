dbfcf4923cde27d33a59304fbcf4e9f19ef11b37
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-14 15:49:37 +0300

    add better definition of break, continue

--- loops/break_and_continue.py
+++ loops/break_and_continue.py
@@ -1,8 +1,10 @@
-print("for loops iterate over every element in a sequence and while loops continue with iterations until their stop condition is met. This is how we choose where to use the two depending on the kind of functionality we aim to get")
+print("for loops iterate over every element in an iterable and while loops continue with iterations until their stop condition is met. This is how we choose where to use the two depending on the kind of functionality we aim to get")
 print("However, sometimes we need to have more precise control over when our loops stop")
 print("This is where the 'break' keyword comes in. break will terminate a for or while loop whenever encountered")
+print("The break keyword terminates the loop containing it. Control of the program flows to the statement immediately after the body of the loop")
+print("If the break keyword is inside a nested loop (i.e loop inside another loop), it will only terminate the innermost loop")
 print('#' * 99)
-print("Suppose you want to load a ship with a list of items called manifest which contains tuples as elements each with the name of an item and it's weight. Ideally, you would want to load all items to the ship but the ship has a weight limit which you should not exceed. So you need to load the items whilst checking that the weight has not been exceeded and if it has, you need to stop loading( stop the loop)")
+print("Suppose you want to load a ship with a list of items called manifest which contains tuples as elements each with the name of an item and it's weight. Ideally, you would want to load all items to the ship but the ship has a weight limit which you should not exceed. So you need to load the items whilst checking that the weight has not been exceeded and if it has, you need to stop loading (stop the loop)")
 print('#' * 99)
 
 manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
@@ -21,8 +23,8 @@ for cargo in manifest:
 print("current weight ({}) and items loaded {}".format(weight, items))
 print('#' * 99)
 
-print("Sometimes, we also might need to skip an iteration because of some condition. Here we use the continue keyword")
-print("continue skips an iteration of a loop")
+print("Sometimes, we also might need to skip an iteration because of some condition.\nHere we use the 'continue' keyword")
+print("The continue statement is used to stop the rest of the code inside a loop for the current iteration only. The loop does not terminate but continues on with the next iteration")
 print("Suppose, for example we had a basket of food and wanted to add only those that are fruits to another basket. Looping through the list of food, if we encounter a non-fruit, we are to skip it and not add it to our fruits list then continue looping if there are more food items")
 print('#' * 99)
 fruits = ['apple', 'orange', 'mango', 'pear']
