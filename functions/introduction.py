70c8038f935962375262c745557169d888b6ec52
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-17 10:26:55 +0300

    fix typos

--- functions/introduction.py
+++ functions/introduction.py
@@ -1,4 +1,5 @@
 print("Functions are useful chunks of code that let us encapsulate a task")
+print("Functions are like mini programs within a program")
 print("Encapsulation is a way to perform a whole series of steps using a single command")
 print("In programming, functions encapsulate all the steps of a process into one command")
 print('#' * 99)
@@ -9,18 +10,18 @@ def volume_cylinder(height, radius):
     return volume
 print('#' * 99)
 
-print("Then once you have declared a function, you can call it (use it) by using the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
+print("Then once you have declared a function, you can call it (execute it) by using the name of the function and passing in the required arguments in the parantheses e.g\nprint(volume_cylinder(45, 450))")
 print(volume_cylinder(45, 450))
 
 print('#' * 99)
 print("Defining a function is as easy as above. It has a few important parts as follows:")
-print("A. Function header - This is the first statement in a function definition. It consists of;")
+print("A. Function header (def statements) - This is the first statement in a function definition. It consists of;")
 print("1. 'def' keyword- A function definition always begins with the def keyword. This tells Python that whatever follows is a function definition. That statement that starts with def keyword must always end with a colon (:)")
 print('#' * 99)
 print("2. function name- After the def keyword, what follows is the name of the function. This is the command we will call to run the code encapsulated in it. The rules for naming a function are similar to those of naming Python variables.")
 
 print('#' * 99)
-print("3. Immediately after the function name is a parantheses that houses any number of arguments for the function. Arguments are variables that the function will use in it's body to generate output. They are the inputs that the function will accept when called. Arguments are not mandatory and a function can have none. In this case, the parantheses is left blank")
+print("3. Immediately after the function name is a parantheses that houses any number of parameters for the function. Parameters are variables that will store arguments supplied to the function when it is called. They are the inputs that the function will accept when called. Parameters are not mandatory and a function can have none. In this case, the parantheses is left blank")
 
 print('#' * 99)
 print("B. Function body - This is the indented block of code below the function header")
@@ -34,6 +35,9 @@ def greetings():
 print('#' * 99)
 print("calling the function as follows\nprint(greetings()) we get the output:")
 print(greetings())
+
+print('#' * 99)
+
 
 print('#' * 99)
 print("We get back the code block inside the function body and the None keyword. Why is the None keyword given as an output?")
