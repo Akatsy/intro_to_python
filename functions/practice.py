9f121785ed9af58a3d3483957dfbe4382a20df04
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-18 12:50:27 +0300

    the collatz sequence

--- functions/practice.py
+++ functions/practice.py
@@ -35,3 +35,22 @@ print(readable_timedelta(15))
 print(readable_timedelta(100))
 print(readable_timedelta(366))
 print(readable_timedelta(365))
+
+print('#' * 99)
+
+# The Collatz Sequence
+# Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
+
+# Then write a program that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)
+
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+    elif number % 2 == 1:
+        return 3 * number + 1
+
+num = int(input("Enter any integer value: "))
+print("The collatz sequence for {} is".format(num))
+while num != 1:
+    num = collatz(num)
+    print(num)
