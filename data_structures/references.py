0555f2006a50df924dab0c21af4a43c5b829bddc
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-19 18:58:50 +0300

    references for mutable data types

--- data_structures/references.py
+++ data_structures/references.py
@@ -14,3 +14,15 @@ print("Python does not copy the value stored in a and store it to b. Rather, Pyt
 print('#' * 79)
 a = 100
 print("If you later change the value in a like so a = 100, what you are doing is creating a new value 100 in memory with a completely different reference (id) and assigning that new id to the variable a. This does not then change the reference stored in b, so id(a) == id(b) will result in {} ".format(id(a) == id(b)))
+
+print("Integers are immutable values that do not change")
+print('#' * 79)
+print("The behaviour for mutable values is a bit different")
+a = [1,2,3,4]
+b = a
+a[1] = 5
+print("Consider the following:\n a = [1,2,3,4] \n b = a \n a[1] = 5 \n print(a) \n print(b)")
+print(a)
+print(b)
+print("Why does the change in a affect b??")
+print("When we create the list, we assign its reference to the variable a. Then we assign a to b, in effect copying the reference for the list to variable b. Note that this means a and b thus refer to the same underlying list. So when you modify a, you are modifying the same list b refers to and hence the results above")
