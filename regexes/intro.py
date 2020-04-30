b721464e8ea5815aa5e8b20447a5aa33a55d5c4e
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-30 15:20:14 +0300

    regex definition

---
+++ regexes/intro.py
@@ -0,0 +1,4 @@
+print("Regular expressions (or regexes in short) are descriptions for a pattern of text.")
+print("A regular expression specifies a set of strings (a sequence of characters) that matches it.")
+print("A regex is a sequence of characters that define a search pattern")
+print("For example, \d in a regex stands for a digit character i.e any single numeral character from 0-9 will match this regex")
