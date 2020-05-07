2e163f56225ff6d966e414a9687cc8b6511c8ab5
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-07 20:55:23 +0300

    some of pyinputplus useful functions

--- input_validation/intro.py
+++ input_validation/intro.py
@@ -39,3 +39,23 @@ print("If a user ever enters an invalid input of the kind of data specified by a
 print("In addition to re-prompting, PyInputPlus has other useful features e.g a limit of the number of times to re-prompt user for input, a timeout feature if the user is required to respond within a specific time duration")
 
 print('#' * 99)
+
+print("PyInputPlus Functions".center(99, '='))
+pyip_functions = {
+    'inputStr()': "Prompts the user to enter any string input. This is similar to Python’s input() function but with PyInputPlus’s additional features such as timeouts, retry limits, stripping, allowlist/blocklist, etc",
+    'inputNum()': "Prompts the user to enter a number, either an integer or a floating-point value. Returns an int or float value (depending on if the user entered a decimal in their input.)",
+    'inputChoice()': "Prompts the user to enter one of the provided choices. Returns the selected choice as a string.",
+    'inputMenu()': "Prompts the user to enter one of the provided choices. Also displays a small menu with bulleted, numbered, or lettered options. Returns the selected choice as a string.",
+    'inputDatetime()': "Prompts the user to enter a datetime, formatted as a strptime-format in the formats list. Returns a datetime.datetime object.",
+    'inputYesNo': "Prompts the user to enter a yes/no response. The user can also enter y/n and use any case. Returns the yesVal or noVal argument (which default to 'yes' and 'no'), depending on the user’s selection.",
+    'inputBool()': "Prompts the user to enter a True/False response. The user can also enter t/f and in any case. Returns a boolean value.",
+    'inputEmail()': "Prompts the user to enter an email address. Returns the email address as a string",
+    'inputFilepath()': "Prompts the user to enter a filepath. If mustExist is True, then this filepath must exist on the local filesystem. Returns the filepath as a string",
+    'inputPassword()': "Prompts the user to enter a password. Mask characters will be displayed instead of the actual characters"
+}
+import pprint
+print(pprint.pformat(pyip_functions))
+
+print('#' * 99)
+
+print('#' * 99)
