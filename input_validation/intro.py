5bcd4ddf977e8dc555a83ebbd2343f22375a3072
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-07 18:50:53 +0300

    an example of input validation

--- input_validation/intro.py
+++ input_validation/intro.py
@@ -7,4 +7,23 @@ print("Input validation also can help avoid security vulnerabilities")
 print("For example say you were implementing a withdraw_from_account() function and the user had to input the amount they want to withdraw. You have to make sure that the user does not enter a negative amount because subtracting a negative amount from the account would end up actually adding.Therefore, the 'withdrawal' would end up adding money to the account instead of subtracting")
 
 print('#' * 99)
+
+# input validation example
+while True:
+    age = input("Enter your age:\n")
+    try:
+        age = int(age)
+    except:
+        print("Please input a number in digits")
+        continue
+    if age < 1:
+        print("Age cannot be negative. Input a positive number")
+        continue
+    elif age > 150:
+        print("You cannot be that old. Come on!!")
+        continue
+    else:
+        break
+print(f'Hey {age} years old human!')
+
 print('#' * 99)
