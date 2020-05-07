46f056ba0b9ba32d6a81f5d0a1eb29e3aaf0b442
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-07 20:23:19 +0300

    pyinputplus module

--- input_validation/intro.py
+++ input_validation/intro.py
@@ -9,21 +9,33 @@ print("For example say you were implementing a withdraw_from_account() function 
 print('#' * 99)
 
 # input validation example
-while True:
-    age = input("Enter your age:\n")
-    try:
-        age = int(age)
-    except:
-        print("Please input a number in digits")
-        continue
-    if age < 1:
-        print("Age cannot be negative. Input a positive number")
-        continue
-    elif age > 150:
-        print("You cannot be that old. Come on!!")
-        continue
-    else:
-        break
-print(f'Hey {age} years old human!')
+# while True:
+#     age = input("Enter your age:\n")
+#     try:
+#         age = int(age)
+#     except:
+#         print("Please input a number in digits")
+#         continue
+#     if age < 1:
+#         print("Age cannot be negative. Input a positive number")
+#         continue
+#     elif age > 150:
+#         print("You cannot be that old. Come on!!")
+#         continue
+#     else:
+#         break
+# print(f'Hey {age} years old human!')
+
+print('#' * 99)
+
+print("PyInputPlus".center(99, '='))
+print("PyInputPlus is a Python3 module to provide input()-like functions with additional validation features")
+print("PyInputPlus is not part of the Python Standard Library thus has to be installed separately using pip as follows\n 'pip3 install pyinputplus' ")
+print("The module contains functions similar to the standard Python input() function for different kinds of data e.g email addresses, dates, numbers e.t.c")
+
+print('#' * 99)
+
+print("If a user ever enters an invalid input of the kind of data specified by any of the PyInputPlus input functions, eg a wrongly formatted date, PyInputPlus will re-prompt them to enter the input again")
+print("In addition to re-prompting, PyInputPlus has other useful features e.g a limit of the number of times to re-prompt user for input, a timeout feature if the user is required to respond within a specific time duration")
 
 print('#' * 99)
