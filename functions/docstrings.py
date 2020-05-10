d1313f5b3391e65d033009cc0724cd8740ed54d9
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-10 17:22:50 +0300

    multi-line docstrings

--- functions/docstrings.py
+++ functions/docstrings.py
@@ -29,7 +29,32 @@ print("3. There's no blank line either before or after the docstring")
 print('4. The docstring is a phrase ending in a period. It prescribes the function or method\'s effect as a command ("Do this", "Return that"), not as a description; e.g. don\'t write "Returns the pathname ...".')
 
 print('#' * 99)
+
+print("Multi-line docstrings consist of a summary line just like a one-line docstring, followed by a blank line, followed by a more elaborate description.")
+print("The summary line may be used by automatic indexing tools; it is important that it fits on one line and is separated from the rest of the docstring by a blank line.")
+print("The summary line may be on the same line as the opening quotes or on the next line. The entire docstring is indented the same as the quotes at its first line")
+
+
 print('#' * 99)
+
+print('''The docstring of a script (a stand-alone program) should be usable as its "usage" message, printed when the script is invoked with incorrect or missing arguments (or perhaps with a "-h" option, for "help"). Such a docstring should document the script's function and command line syntax, environment variables, and files. Usage messages can be fairly elaborate (several screens full) and should be sufficient for a new user to use the command properly, as well as a complete quick reference to all options and arguments for the sophisticated user.''')
+
+print('#' * 99)
+
+print('''The docstring for a module should generally list the classes, exceptions and functions (and any other objects) that are exported by the module, with a one-line summary of each. (These summaries generally give less detail than the summary line in the object's docstring.) The docstring for a package (i.e., the docstring of the package's __init__.py module) should also list the modules and subpackages exported by the package.''')
+
+print('#' * 99)
+
+print('''The docstring for a function or method should summarize its behavior and document its arguments, return value(s), side effects, exceptions raised, and restrictions on when it can be called (all if applicable). Optional arguments should be indicated. It should be documented whether keyword arguments are part of the interface.''')
+
+print('#' * 99)
+
+print('''The docstring for a class should summarize its behavior and list the public methods and instance variables. If the class is intended to be subclassed, and has an additional interface for subclasses, this interface should be listed separately (in the docstring). The class constructor should be documented in the docstring for its __init__ method. Individual methods should be documented by their own docstring.
+
+If a class subclasses another class and its behavior is mostly inherited from that class, its docstring should mention this and summarize the differences. Use the verb "override" to indicate that a subclass method replaces a superclass method and does not call the superclass method; use the verb "extend" to indicate that a subclass method calls the superclass method (in addition to its own behavior)''')
+
+print('#' * 99)
+
 
 
 print("The first line of a docstring gives a brief description of what the function does. This is enough if the function is not that complicated")
