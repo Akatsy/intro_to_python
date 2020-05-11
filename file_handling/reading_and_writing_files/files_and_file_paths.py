bda6ea939367917db6d3289b96c24f0be6878c59
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-11 11:13:14 +0300

    properties of a file

--- file_handling/reading_and_writing_files/files_and_file_paths.py
+++ file_handling/reading_and_writing_files/files_and_file_paths.py
@@ -1,4 +1,13 @@
 print("Variables store data when a program is running")
 print("Files persist data after a program has stopped running")
 print("File's content - Think of it as a single string value potentially gigabytes in size")
+
 print('' * 99)
+
+print("A file has two key properties:")
+print("1. filename - this is the name of the file (usually written as one word) - Additionally (optional in some OS like Linux) the part of the filename after the last period character is called the file's extension and tells you the file's type i.e what kind of file it is e.g .py specifies it is a python file")
+print("2. path - This specifies the location of a file on the computer")
+
+print('' * 99)
+print('' * 99)
+
