7f4c4b8db5eee32a67080338c2ff83a989e32fd1
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-11 11:02:34 +0300

    files vs variables

---
+++ file_handling/reading_and_writing_files/files_and_file_paths.py
@@ -0,0 +1,4 @@
+print("Variables store data when a program is running")
+print("Files persist data after a program has stopped running")
+print("File's content - Think of it as a single string value potentially gigabytes in size")
+print('' * 99)
