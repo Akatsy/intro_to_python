afd2564ea6e35e5c2fe743553ccd272ed318da48
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-14 20:08:15 +0300

    two main types of files

---
+++ file_handling/reading_and_writing_files/file_reading_and_writing_process.py
@@ -0,0 +1,5 @@
+print(r'''
+There are two types of files:
+plain text files - These are files that contain only basic text characters.
+binary files - These are all the other kinds of files that contain extra information like font, color, size e.t.c e.g word processing documents, PDFs, images, spreadsheets and executable programs among others
+''')
