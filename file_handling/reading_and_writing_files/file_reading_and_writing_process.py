9fe1c09a15684001c18b938800440d21f6ba7a5f
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-14 22:36:19 +0300

    path object read_text(), write_text() methods

--- file_handling/reading_and_writing_files/file_reading_and_writing_process.py
+++ file_handling/reading_and_writing_files/file_reading_and_writing_process.py
@@ -3,3 +3,18 @@ There are two types of files:
 plain text files - These are files that contain only basic text characters.
 binary files - These are all the other kinds of files that contain extra information like font, color, size e.t.c e.g word processing documents, PDFs, images, spreadsheets and executable programs among others
 ''')
+
+print('#' * 99)
+
+print(r'''
+pathlib's read_text() method returns a string of the full contents of a text file
+pathlib's write_text() creates a new text file (or overwrites an existing one) with the string passed to it. It returns a count of the number of characters written to the file
+These path objects methods only provide basic interactions with files and are not the common way of writing to a file
+
+''')
+from pathlib import Path
+p = Path('/home/mutwiri2/Desktop/intro_to_python/file_handling/reading_and_writing_files/try.txt')
+written_count = p.write_text('Hello, World!')
+print(written_count)
+content = p.read_text()
+print(content)
