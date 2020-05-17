4fb76e1c3cd0bb0e9f926c751673fe34e41725d1
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-17 17:04:45 +0300

    solution to quiz

--- file_handling/intro.py
+++ file_handling/intro.py
@@ -193,12 +193,16 @@ print('#' * 99)
 def create_cast_list(filename):
     cast_list = []
     #use with to open the file filename
+    with open(filename) as f:
+        for line in f:
+            name = line.split(',')
+            cast_list.append(name[0])
     #use the for loop syntax to process each line
     #and add the actor name to cast_list
 
     return cast_list
 
-cast_list = create_cast_list('flying_circus_cast.txt')
+cast_list = create_cast_list('/home/mutwiri2/Desktop/intro_to_python/file_handling/flying_circus_cast.txt')
 for actor in cast_list:
     print(actor)
 
