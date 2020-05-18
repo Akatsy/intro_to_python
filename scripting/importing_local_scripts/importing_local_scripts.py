d3a18268655bcc9b7e09f27dc06f8737b4120264
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-18 09:07:21 +0300

    how to access objects in an imported script

--- scripting/importing_local_scripts/importing_local_scripts.py
+++ scripting/importing_local_scripts/importing_local_scripts.py
@@ -4,3 +4,12 @@ If the Python file you want to import is in the same folder as your current scri
 Import statements are written at the top of a Python script, each one on a separate line although Python will still run if they are included later in the script (Note: You are only able to access what you have imported after the import statement)
 Having the import statement at the top of a file is nice as readers get to see what your script depends on before reading the rest of the code.
 ''')
+
+print('#' * 99)
+
+print('''
+To access objects in other scripts from our current script, we cannot just type the name of the object - this will raise a NameError exception. 
+We need to use the name of the file (module) we imported followed by a dot (.) then the object name - This tells Python to look for the object in that file
+When Python runs a script, it only has direct access to objects defined in that script. If the script has an import statement, then one of the objects the current script has access to is the module object created by the import statement. The name of the object is the name given after the import keyword and the object is of type module
+A module is just a file with Python definitions and statements
+''')
