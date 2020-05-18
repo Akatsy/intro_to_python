7d5eaaf7be2a380c77755313b76bb1c34a04078f
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-18 10:21:11 +0300

    executable statements inside main

--- scripting/importing_local_scripts/useful_functions.py
+++ scripting/importing_local_scripts/useful_functions.py
@@ -14,3 +14,17 @@ if __name__ == '__main__':
     assert(add_five(n_list) == correct_added_list)
 
     print("Tests passed!")
+
+def main():
+    print("Testing functions")
+    n_list = [10, 20, 40, 60, 80]
+    correct_mean  = 42
+    assert(mean(n_list) == correct_mean)
+
+    correct_added_list = [15, 25, 45, 65, 85]
+    assert(add_five(n_list) == correct_added_list)
+
+    print("Tests passed!")
+
+if __name__ == '__main__':
+    main()
