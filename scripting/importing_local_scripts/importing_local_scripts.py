316ffde9b866fe00c12436df694372147613875c
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-18 08:50:30 +0300

    importing local scripts

---
+++ scripting/importing_local_scripts/importing_local_scripts.py
@@ -0,0 +1,6 @@
+print('''
+We can import Python code from other scripts into our programs
+If the Python file you want to import is in the same folder as your current script, you just type import followed by the name of the file without the .py extension
+Import statements are written at the top of a Python script, each one on a separate line although Python will still run if they are included later in the script (Note: You are only able to access what you have imported after the import statement)
+Having the import statement at the top of a file is nice as readers get to see what your script depends on before reading the rest of the code.
+''')
