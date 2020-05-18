b2791228f8c29bbc9c442adccce2551f69d8ae21
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-18 10:15:16 +0300

    add executable statements to script

--- scripting/importing_local_scripts/useful_functions.py
+++ scripting/importing_local_scripts/useful_functions.py
@@ -3,3 +3,13 @@ def mean(num_list):
 
 def add_five(num_list):
     return [n+5 for n in num_list]
+
+print("Testing functions")
+n_list = [10, 20, 40, 60, 80]
+correct_mean  = 42
+assert(mean(n_list) == correct_mean)
+
+correct_added_list = [15, 25, 45, 65, 85]
+assert(add_five(n_list) == correct_added_list)
+
+print("Tests passed!")
