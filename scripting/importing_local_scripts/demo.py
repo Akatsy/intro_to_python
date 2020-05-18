87d17a3af39fc63d41feac0e277b72718508f166
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-18 09:10:51 +0300

    accessing an object in another script

--- scripting/importing_local_scripts/demo.py
+++ scripting/importing_local_scripts/demo.py
@@ -1,2 +1,10 @@
 import other_script
 print(5*5)
+try:
+    print(greeting)
+except Exception as e:
+    print(e)
+finally:
+    print("The above is to demonstrate you cannot access an object from another script directly using just it's identifier")
+
+print(other_script.greeting)
