83be993cd091b4694fdb5d33fa105b2ae686f9a1
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-20 18:58:15 +0300

    iterator vs iterable

---
+++ iterators_and_generators/iterators_and_generators.py
@@ -0,0 +1,7 @@
+# generator functions and iterators
+
+print(r'''
+Iterables are objects that can return each of their elements one at a time e.g a list
+Iterator is an object that represents a stream of data for example one returned by the enumerate function
+An iterator is different from an iterable like a list since a list is not a stream of data
+''')
