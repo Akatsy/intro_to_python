a91e7b66902c79663340cae37207cff3b32e81d0
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-20 19:21:47 +0300

    generator

--- iterators_and_generators/iterators_and_generators.py
+++ iterators_and_generators/iterators_and_generators.py
@@ -4,4 +4,7 @@ print(r'''
 Iterables are objects that can return each of their elements one at a time e.g a list
 Iterator is an object that represents a stream of data for example one returned by the enumerate function
 An iterator is different from an iterable like a list since a list is not a stream of data
+
+You can create iterators using generators
+Generator - a function that creates an iterator
 ''')
