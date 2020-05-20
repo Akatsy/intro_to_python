fa75fc064e4629849f1b4d39f8b8f6a424329a68
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-20 22:10:00 +0300

    yield successive chunks from iterable

--- iterators_and_generators/chunker.py
+++ iterators_and_generators/chunker.py
@@ -18,12 +18,9 @@
 # [24]
 
 def chunker(iterable, size):
-    x = 0
-    iterations = 
-    while x < size:
-        yield x
-        x += 1
-
+    """Yield successive chunks from iterable of length size."""
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
 
 
 for chunk in chunker(range(25), 4):
