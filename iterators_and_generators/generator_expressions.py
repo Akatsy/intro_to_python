d6c849ad94f422bfa397c674352824f828601d96
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-24 20:30:26 +0300

    generator expressions have lazy execution - memory efficient

--- iterators_and_generators/generator_expressions.py
+++ iterators_and_generators/generator_expressions.py
@@ -1,9 +1,23 @@
 print(r'''
-Generator Expressions
-Here's a cool concept that combines generators and list comprehensions! You can actually create a generator in the same way you'd normally write a list comprehension, except with parentheses instead of square brackets. For example:
+
+Python Generator Expressions
+Simple generators can be easily created on the fly using generator expressions. It makes building generators easy.
+
+Similar to the lambda functions which create anonymous functions, generator expressions create anonymous generator functions.
+
+The syntax for generator expression is similar to that of a list comprehension in Python. But the square brackets are replaced with round parentheses.
+
+The major difference between a list comprehension and a generator expression is that a list comprehension produces the entire list while the generator expression produces one item at a time.
+
+They have lazy execution ( producing items only when asked for ). For this reason, a generator expression is much more memory efficient than an equivalent list comprehension.
 
 sq_list = [x**2 for x in range(10)]  # this produces a list of squares
 
-sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
+sq_generator = (x**2 for x in range(10))  # this produces an iterator of squares
 This can help you save time and create efficient code!
 ''')
+
+sq_list = [x**2 for x in range(100)]
+print(sq_list)
+sq_generator = (x**2 for x in range(100))
+print(sq_generator)
