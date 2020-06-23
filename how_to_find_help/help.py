b9c2953b3ad21c66528006d5a8f58c26e30da6fa
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-23 12:37:21 +0300

    limit each line to 79 characters

--- how_to_find_help/help.py
+++ how_to_find_help/help.py
@@ -1,40 +1,61 @@
 print(r'''
 Programmers tend to learn by searching the internet for answers to their questions.
-What’s great about using the internet as a schoolroom is that there are whole communities of folks who can answer your questions.
-Indeed, your questions have probably already been answered, and the answers are waiting online for you to find them.
-If you encounter an error message or have trouble making your code work, you won’t be the first person to have that problem, and finding a solution is easier than you might think.
+What’s great about using the internet as a schoolroom is that there are whole
+communities of folks who can answer your questions.
+Indeed, your questions have probably already been answered, and the answers are 
+waiting online for you to find them.
+If you encounter an error message or have trouble making your code work, you
+won’t be the first person to have that problem, and finding a solution is easy
 
- No one person can know everything about programming, so an everyday part of any software developer’s job is looking up answers to technical questions.
+No one person can know everything about programming, so an everyday part of any
+software developer’s job is looking up answers to technical questions.
 
-##########################################################################################################################
+################################################################################
 
 Asking Smart Programming Questions
 
-If you can’t find the answer by searching online, try asking people in a web forum such as Stack Overflow (https://stackoverflow.com/) or the “learn programming” subreddit at https://reddit.com/r/learnprogramming/
+If you can’t find the answer by searching online, try asking people in a web
+forum such as Stack Overflow (https://stackoverflow.com/) or the 
+“learn programming” subreddit at https://reddit.com/r/learnprogramming/
 
-But keep in mind there are smart ways to ask programming questions that help others help you.
-To begin with, be sure to read the FAQ sections at these websites about the proper way to post questions.
+There are smart ways to ask programming questions that help others help you.
+First, be sure to read the FAQ sections at these websites about the proper way
+to post questions.
 
 When asking programming questions, remember to do the following:
 
-1. Explain what you are trying to do, not just what you did. This lets your helper know if you are on the wrong track.
-Specify the point at which the error happens. Does it occur at the very start of the program or only after you do a certain action?
+1. Explain what you are trying to do, not just what you did. This lets your 
+helper know if you are on the wrong track.
+Specify the point at which the error happens. Does it occur at the very start of
+the program or only after you do a certain action?
 
-2. Copy and paste the entire error message and your code to https://pastebin.com/ or https://gist.github.com/.
-These websites make it easy to share large amounts of code with people online, without losing any text formatting. You can then put the URL of the posted code in your email or forum post.
+2. Copy and paste the entire error message and your code to
+https://pastebin.com/ or https://gist.github.com/.
+These websites make it easy to share large amounts of code with people online, 
+without losing any text formatting. You can then put the URL of the posted code 
+in your email or forum post.
 
-3. Explain what you’ve already tried to do to solve your problem. This tells people you’ve already put in some work to figure things out on your own.
+3. Explain what you’ve already tried to do to solve your problem. This tells 
+people you’ve already put in some work to figure things out on your own.
 
-4. List the version of Python you’re using. (There are some key differences between version 2 Python interpreters and version 3 Python interpreters.) Also, say which operating system and version you’re running.
+4. List the version of Python you’re using. (There are some key differences 
+between version 2 Python interpreters and version 3 Python interpreters.) Also, 
+say which operating system and version you’re running.
 
-5. If the error came up after you made a change to your code, explain exactly what you changed.
+5. If the error came up after you made a change to your code, explain exactly 
+what you changed.
 
-6. Say whether you’re able to reproduce the error every time you run the program or whether it happens only after you perform certain actions. If the latter, then explain what those actions are.
+6. Say whether you’re able to reproduce the error every time you run the program
+or whether it happens only after you perform certain actions. If the latter, 
+then explain what those actions are.
 
 
-#####################################################################################################################
+################################################################################
 
-You can find more information on how to ask for programming help in the blog post at https://autbor.com/help/.
-You can find a list of frequently asked questions about programming at https://www.reddit.com/r/learnprogramming/wiki/faq/
-You can find a list about getting a job in software development at https://www.reddit.com/r/cscareerquestions/wiki/index/.
+You can find more information on how to ask for programming help in the blog post
+at https://autbor.com/help/.
+You can find a list of frequently asked questions about programming 
+at https://www.reddit.com/r/learnprogramming/wiki/faq/
+You can find a list about getting a job in software development 
+at https://www.reddit.com/r/cscareerquestions/wiki/index/.
 ''')
