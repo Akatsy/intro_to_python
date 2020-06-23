82b43ce56c9e581c47ebfa3008272a8e9e945574
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-23 12:30:17 +0300

    add line breaks to limit characters to 79 per line

--- intro.py
+++ intro.py
@@ -1,34 +1,58 @@
-print("Python is an interpreted, high-level, general-purpose programming language created by Guido van Rossum and first released in 1991")
+print("Python is an interpreted, high-level, general-purpose programming language\
+ created by Guido van Rossum and first released in 1991")
 print('#' * 99)
-print("Interpreted means that most of its implementations execute instructions directly and freely without previously compiling a program into machine-language instructions")
+print("Interpreted means that most of its implementations execute instructions\
+ directly and freely without previously compiling a program into machine-language\
+ instructions")
 print('#' * 99)
-print("high-level means that it is a programming language with strong abstraction from the details of the computer")
+print("high-level means that it is a programming language with strong abstraction\
+ from the details of the computer")
 print('#' * 99)
-print("In contrast to a low-level programming language, a high-level programming language has the following characteristics:")
+print("In contrast to a low-level programming language, a high-level programming\
+ language has the following characteristics:")
 print("1. It may use natural language elements e.g english-like words like 'print'")
-print("2. May automate (or hide entirely) significant areas of computing systems like memory management")
-print("The above characteristics make high-level languages easier to use and makes the process of developing a program simpler and more understandable")
+print("2. May automate (or hide entirely) significant areas of computing systems\
+ like memory management")
+print("The above characteristics make high-level languages easier to use and makes\
+ the process of developing a program simpler and more understandable")
 print('#' * 99)
-print("General-purpose means that it is a programming language designed to be used for writing software in the widest variety of application domains. This is because it does not include language constructs designed to be used within a specific application domain")
+print("General-purpose means that it is a programming language designed to be used\
+ for writing software in the widest variety of application domains. This is because\
+ it does not include language constructs designed to be used within a specific\
+ application domain")
 print('#' * 99)
 
-print("Python's design philosophy emphasizes code readability with its notable use of significant whitespaces")
-print("The language's core philosophy is summarized in the document 'The Zen of Python'")
+print("Python's design philosophy emphasizes code readability with its notable\
+ use of significant whitespaces")
+print("The language's core philosophy is summarized in the document 'The Zen of\
+ Python'")
 print('#' * 99)
-print("The Zen of Python is a collection of '19 guiding principles' for writing computer programs that influence the design of the Python programming language")
-print("It is included as an easter egg in the Python interpreter which can be displayed by typing the following statement on the Python interpreter:\nimport this\n")
+print("The Zen of Python is a collection of '19 guiding principles' for writing\
+ computer programs that influence the design of the Python programming language")
+print("It is included as an easter egg in the Python interpreter which can be\
+ displayed by typing the following statement on the Python interpreter:\nimport this\n")
 import this
 
 print('#' * 99)
-print("Python is a powerful, general-purpose programming language with the following applications:")
+print("Python is a powerful, general-purpose programming language with the\
+ following applications:")
 print("web and internet development")
-print("scientific and numeric - Python is widely used in scientific and numeric computing")
+print("scientific and numeric - Python is widely used in scientific and numeric\
+ computing")
 print("creating Desktop GUIs")
-print("Software and game development - Python is often used as a support language for software developers, for build control and management, testing, and in many other ways.")
-print("Education - Python is a superb language for teaching programming, both at the introductory level and in more advanced courses.")
-print("scripting - writing code used to automate processes that would otherwise need to be executed step-by-step by a developer")
+print("Software and game development - Python is often used as a support language\
+ for software developers, for build control and management, testing e.t.c")
+print("Education - Python is a superb language for teaching programming, both at\
+ the introductory level and in more advanced courses.")
+print("scripting - writing code used to automate processes that would otherwise\
+ need to be executed step-by-step by a developer")
 print('#' * 99)
 print("A few points to note before learning Python:")
 print("1. Python is case-sensitive so 'name' and 'Name' are not the same")
-print("2. Spacing matters in Python - Python is not heavy on syntax so it relies heavily on spacing to delimit code blocks")
-print("3. You will inevitably run into errors as you learn and code in Python. Errors do not make you a bad programmer. It is just a way for the computer to tell you that it does not quite understand what you have instructed it to do. Use errors as a learning tool, don't let them be a deterrent or discouragement in your learning to code journey")
+print("2. Spacing matters in Python - Python is not heavy on syntax so it relies\
+ heavily on spacing to delimit code blocks")
+print("3. You will inevitably run into errors as you learn and code in Python.\
+ Errors do not make you a bad programmer. It is just a way for the computer to\
+ tell you that it does not quite understand what you have instructed it to do.\
+ Use errors as a learning tool, don't let them be a deterrent or discouragement\
+ in your learning to code journey")
