afcdeeb0af28e7cf6071ae35b0eaf23ea341d41b
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-29 16:38:48 +0300

    dir() and help() built-in functions

--- datatypes_and_operators/dir_and_help_functions.py
+++ datatypes_and_operators/dir_and_help_functions.py
@@ -1,16 +1,31 @@
-# quickly get valid attributes of an object in Python using dir() and their definitions using help() built-in functions
-print("The dir() built-in fuction returns information about an object. It can be useful when you have an object and want to quickly find out what you can do with it. It returns some of the attributes and methods of that object (Remember every object you encounter in Python has a type and this is what will determine the attributes and methods returned)")
-print('#' * 79)
-num = 5
-print("Say you have an integer like 5 assigned to the variable num, calling dir() on num will result in the following attributes")
-for index, attribute in enumerate(dir(num)):
-    print(index, attribute)
-print('#' * 79)
-print("Calling dir() on a string like dir('s') would result in the following:")
-for index, attrib in enumerate(dir('s')):
-    print(index, attrib)
+# quickly get valid attributes of an object in Python using dir() and their
+# definitions using help() built-in functions
+print("The dir() built-in fuction returns information about an object. It can be\
+ useful when you have an object and want to quickly find out what you can do with\
+ it. It returns some of the attributes and methods of that object (Remember \
+ every object you encounter in Python has a type and this is what will determine\
+ the attributes and methods returned)")
 print('#' * 79)
 
-print("The information printed is not very useful so we have another builtin function called help() which gives a short description of any of the attributes returned by dir(). You call help() on the attribute using the dot notation e.g help(num.__add__) or help('s'.rfind)")
+num = 5
+print("Say you have an integer like 5 assigned to the variable num, calling dir()\
+ on num will result in the following attributes:")
+
+for index, attribute in enumerate(dir(num)):
+    print(index, attribute)
+
+print('#' * 79)
+
+print("Calling dir() on a string like dir('s') would result in the following:")
+
+for index, attrib in enumerate(dir('s')):
+    print(index, attrib)
+
+print('#' * 79)
+
+print("The information printed is not very useful so we have another built-in\
+ function called help() which gives a short description of any of the attributes\
+ returned by dir(). You call help() on the attribute using the dot notation e.g\
+ help(num.__add__) or help('s'.rfind)")
 print('#' * 79)
 # print(help(num.__add__))
