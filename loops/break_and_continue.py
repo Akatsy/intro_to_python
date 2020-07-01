9d9cd1f60d4cdc4271129233c38c6f11578dabf8
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-01 09:59:18 +0300

    limit lines to between 79 - 99 characters

--- loops/break_and_continue.py
+++ loops/break_and_continue.py
@@ -1,15 +1,34 @@
-# how to exit from a loop using break keyword and how to skip an iteration of a loop using continue
+# how to exit from a loop using break keyword and how to skip an iteration of a
+#  loop using continue
 
-print("for loops iterate over every element in an iterable and while loops continue with iterations until their stop condition is met. This is how we choose where to use the two depending on the kind of functionality we aim to get")
-print("However, sometimes we need to have more precise control over when our loops stop")
-print("This is where the 'break' keyword comes in. break will terminate a for or while loop whenever encountered")
-print("The break keyword terminates the loop containing it. Control of the program goes to the statement immediately after the body of the loop")
-print("If the break keyword is inside a nested loop (i.e loop inside another loop), it will only terminate the innermost loop")
-print('#' * 99)
-print("Suppose you want to load a ship with a list of items called manifest which contains tuples as elements each with the name of an item and its weight. Ideally, you would want to load all items to the ship but the ship has a weight limit which you should not exceed. So you need to load the items whilst checking that the weight has not been exceeded and if it has, you need to stop loading (stop the loop)")
+print("for loops iterate over every element in an iterable and while loops\
+ continue with iterations until their stop condition is met. This is how we\
+ choose where to use the two depending on the kind of functionality we aim to\
+ get")
+
+print("However, sometimes we need to have more precise control over when our\
+ loops stop")
+
+print("This is where the 'break' keyword comes in.\n break will terminate a for\
+ or while loop whenever encountered")
+
+print("The break keyword terminates the loop containing it. Control of the\
+ program goes to the statement immediately after the body of the loop")
+
+print("If the break keyword is inside a nested loop (i.e loop inside another \
+loop), it will only terminate the innermost loop")
 print('#' * 99)
 
-manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
+print("Suppose you want to load a ship with a list of items called manifest which\
+ contains tuples as elements each with the name of an item and its weight. Ideally,\
+ you would want to load all items to the ship but the ship has a weight limit\
+ which you should not exceed. So you need to load the items whilst checking that\
+ the weight has not been exceeded and if it has, you need to stop loading\
+ (stop the loop)")
+print('#' * 99)
+
+manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42),\
+ ("machine", 120), ("cheeses", 5)]
 weight = 0
 items = []
 weight_limit = 100
@@ -25,10 +44,19 @@ for cargo in manifest:
 print("current weight ({}) and items loaded {}".format(weight, items))
 print('#' * 99)
 
-print("Sometimes, we also might need to skip an iteration because of some condition.\nHere we use the 'continue' keyword")
-print("The continue statement is used to stop the rest of the code inside a loop for the current iteration only. The loop does not terminate but continues on with the next iteration")
-print("Suppose, for example we had a basket of food and wanted to add only those that are fruits to another basket. Looping through the list of food, if we encounter a non-fruit, we are to skip it and not add it to our fruits list then continue looping if there are more food items")
+print("Sometimes, we also might need to skip an iteration because of some\
+ condition.\nHere we use the 'continue' keyword")
+
+print("The continue statement is used to stop the rest of the code inside a loop\
+ for the current iteration only. The loop does not terminate but continues on\
+ with the next iteration")
+
+print("Suppose, for example we had a basket of food and wanted to add only those\
+ that are fruits to another basket. Looping through the list of food, if we\
+ encounter a non-fruit, we are to skip it and not add it to our fruits list then\
+ continue looping if there are more food items")
 print('#' * 99)
+
 fruits = ['apple', 'orange', 'mango', 'pear']
 food = ['cake', 'apple', 'bread', 'pear', 'juice', 'orange', 'beef', 'mango']
 basket = []
@@ -43,7 +71,8 @@ print("our basket contains {}".format(basket))
 print('#' * 99)
 
 # solve the manifest bugs using continue
-manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
+manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42),\
+ ("machine", 120), ("cheeses", 5)]
 weight = 0
 items = []
 weight_limit = 100
@@ -52,7 +81,8 @@ for cargo_item, cargo_weight in manifest:
         print("weight limit reached, loading stopped")
         break
     elif weight + cargo_weight > weight_limit:
-        print("adding item {}, weight {} will exceed the weight limit of {}, skipping item...".format(cargo_item, cargo_weight, weight_limit))
+        print("adding item {}, weight {} will exceed the weight limit of {},\
+         skipping item...".format(cargo_item, cargo_weight, weight_limit))
         continue
     else:
         print("adding {} weight {} to ship".format(cargo_item, cargo_weight))
@@ -62,7 +92,11 @@ print("Items addded to ship are {}, total weight of items is {}".format(items, w
 
 print('#' * 99)
 
-# Write a loop with a break statement to create a string, news_ticker, that is exactly 140 characters long. You should create the news ticker by adding headlines from the headlines list, inserting a space in between each headline. If necessary, truncate the last headline in the middle so that news_ticker is exactly 140 characters long.
+# Write a loop with a break statement to create a string, news_ticker, that is
+#  exactly 140 characters long. You should create the news ticker by adding
+#  headlines from the headlines list, inserting a space in between each headline.
+#  If necessary, truncate the last headline in the middle so that news_ticker is
+#  exactly 140 characters long.
 
 # HINT: modify the headlines list to verify your loop works with different inputs
 headlines = ["Local Bear Eaten by Man",
@@ -79,7 +113,8 @@ for headline in headlines:
         print("140 character limit reached...")
         break
     elif len(headline) + 1 + len(news_ticker) > 140:
-        print("adding the headline will exceed the character limit by {}, truncating to fit...".format(len(headline) + 1 + len(news_ticker) - 140))
+        print("adding the headline will exceed the character limit by {},\
+         truncating to fit...".format(len(headline) + 1 + len(news_ticker) - 140))
         headline = headline[:140 - len(news_ticker)]
         news_ticker += headline
     else:
@@ -106,7 +141,8 @@ print("Our newsticker is \"{}\" and has {} characters".format(news_ticker, len(n
 print('#' * 99)
 
 # Coding Quiz: Check for Prime Numbers
-# Prime numbers are whole numbers that have only two factors: 1 and the number itself. The first few prime numbers are 2, 3, 5, 7.
+# Prime numbers are whole numbers that have only two factors: 1 and the number
+# itself. The first few prime numbers are 2, 3, 5, 7.
 
 # For instance, 6 has four factors: 1, 2, 3, 6.
 # 1 X 6 = 6
@@ -114,7 +150,9 @@ print('#' * 99)
 # So we know 6 is not a prime number.
 
 # If the numbers are prime, the code should print "[number] is a prime number."
-# If the number is NOT a prime number, it should print "[number] is not a prime number", and a factor of that number, other than 1 and the number itself: "[factor] is a factor of [number]".
+# If the number is NOT a prime number, it should print "[number] is not a prime
+#  number", and a factor of that number, other than 1 and the number itself: 
+# "[factor] is a factor of [number]".
 
 check_prime = [26, 39, 51, 53, 57, 79, 85, 7]
 
