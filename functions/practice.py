83381deff1c51b7a256a47d5372a5c3d46d0fa1b
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-02 20:43:27 +0300

    limit each line to 79 - 99 characters

--- functions/practice.py
+++ functions/practice.py
@@ -1,11 +1,10 @@
 # Quiz: Population Density Function
-# Write a function named population_density that takes two arguments, population and land_area, and returns a population density calculated from those values.
+# Write a function named population_density that takes two arguments, population
+# and land_area, and returns a population density calculated from those values.
 
 # function
 def population_density(population, land_area):
     return population/land_area
-
-
 
 # test cases for function
 test1 = population_density(10, 1)
@@ -20,7 +19,9 @@ print("expected result: {}, actual result: {}".format(expected_result2, test2))
 print('#' * 99)
 
 # Quiz: readable_timedelta
-# Write a function named readable_timedelta. The function should take one argument, an integer days, and return a string that says how many weeks and days that is. For example, calling the function and printing the result like this:
+# Write a function named readable_timedelta. The function should take one argument,
+# an integer days, and return a string that says how many weeks and days that is.
+# For example, calling the function and printing the result like this:
 
 # print(readable_timedelta(10))
 # should output the following:
@@ -39,9 +40,16 @@ print(readable_timedelta(365))
 print('#' * 99)
 
 # The Collatz Sequence
-# Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
+# Write a function named collatz() that has one parameter named number. If number
+# is even, then collatz() should print number // 2 and return this value. If 
+# number is odd, then collatz() should print and return 3 * number + 1.
 
-# Then write a program that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)
+# Then write a program that lets the user type in an integer and that keeps
+# calling collatz() on that number until the function returns the value 1. 
+# (Amazingly enough, this sequence actually works for any integer—sooner or later,
+# using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why.
+# Your program is exploring what’s called the Collatz sequence, sometimes called
+# “the simplest impossible math problem.”)
 
 def collatz(number):
     if number % 2 == 0:
@@ -71,7 +79,12 @@ print('#' * 99)
 
 # spam = ['apples', 'bananas', 'tofu', 'cats']
 
-# Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted before the last item. For example, passing the previous spam list to the function would return 'apples, bananas, tofu, and cats'. But your function should be able to work with any list value passed to it. Be sure to test the case where an empty list [] is passed to your function.
+# Write a function that takes a list value as an argument and returns a string
+# with all the items separated by a comma and a space, with and inserted before
+# the last item. For example, passing the previous spam list to the function 
+# would return 'apples, bananas, tofu, and cats'. But your function should be
+# able to work with any list value passed to it. Be sure to test the case where
+# an empty list [] is passed to your function.
 
 def comma_seperated_items(arr):
     return_str = ''
@@ -84,4 +97,5 @@ def comma_seperated_items(arr):
 
 spam = ['apples', 'bananas', 'tofu', 'cats']
 print(comma_seperated_items(spam))
-print(comma_seperated_items([])) # comment out the collatz sequence code for this code to run coz of the sys.exit() statement
+print(comma_seperated_items([])) # comment out the collatz sequence code for this
+# code to run coz of the sys.exit() statement
