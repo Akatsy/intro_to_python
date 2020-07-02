47ef4c9cca12b09436829b70ed0456f37df912c8
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-02 20:44:32 +0300

    space to lower priority arithmetic operation

--- functions/practice.py
+++ functions/practice.py
@@ -55,7 +55,7 @@ def collatz(number):
     if number % 2 == 0:
         return number // 2
     elif number % 2 == 1:
-        return 3 * number + 1
+        return 3*number + 1
 
 import sys
 while True:
