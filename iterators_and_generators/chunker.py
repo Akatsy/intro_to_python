4841dc1fb85457e1d2c7290d623d8c6ffba8e7da
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-03 20:21:36 +0300

    limit characters per line to 79 - 99

--- iterators_and_generators/chunker.py
+++ iterators_and_generators/chunker.py
@@ -1,5 +1,6 @@
 # Quiz: Chunker
-# If you have an iterable that is too large to fit in memory in full (e.g., when dealing with large files), being able to take and use chunks of it at a time can be very valuable.
+# If you have an iterable that is too large to fit in memory in full (e.g., when
+# dealing with large files), being able to take and use chunks of it at a time can be very valuable.
 
 # Implement a generator function, chunker, that takes in an iterable and yields a chunk of a specified size at a time.
 
