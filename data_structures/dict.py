ce77a6e9f2ef366a66ad82d866ff4e7f94cfea6d
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-04 21:21:40 +0300

    79 - 99 characters per line

--- data_structures/dict.py
+++ data_structures/dict.py
@@ -1,35 +1,72 @@
 # store mappings of keys to values using the dict data type 
-print("Dictionaries are a data type for a mutable unordered mapping of unique keys to values")
-print("The key has to be unique because it is used to access the value to print it out, or modify it")
-print("The key has to be of an immutable data type like int, float, string, tuple and cannot be of a mutable data type like sets or lists")
-print("Immutable data types are hashable which means their values do not change during their lifetime allowing Python to create hash values for them to uniquely identify them")
+print("Dictionaries are a data type for a mutable unordered mapping of unique\
+ keys to values")
+
+print("The key has to be unique because it is used to access the value to print\
+ it out, or modify it")
+
+print("The key has to be of an immutable data type like int, float, string, tuple\
+ and cannot be of a mutable data type like sets or lists")
+
+print("Immutable data types are hashable which means their values do not change\
+ during their lifetime allowing Python to create hash values for them to uniquely\
+ identify them")
 print('#' * 79)
+
 print("The dict can be created as follows:")
+
 thisdict = {'brand': 'Ford','model': 'Mustang','year': 1964}
-print("By using curly braces '{}' to sorround a comma separated collection of key-value pairs delimited by a colon (:) as follows thisdict = {'brand': 'Ford','model': 'Mustang','year': 1964} and printing it out, you get\n", thisdict)
+print("By using curly braces '{}' to sorround a comma separated collection of\
+ key-value pairs delimited by a colon (:) as follows thisdict = {'brand': 'Ford',\
+'model': 'Mustang','year': 1964} and printing it out, you get\n", thisdict)
 print('#' * 79)
+
 n = {}
 m = dict()
-print("When you declare a variable and assign it an empty set of curly braces, it is assigned the type dict (instead of a set) e.g n = {} and type(n) will give; ", type(n))
-print("When you declare a variable and assign it to the constructor dict() like this m = dict(), it is assigned the type dict and type(m) will give; ", type(m))
+print("When you declare a variable and assign it an empty set of curly braces,\
+ it is assigned the type dict (instead of a set) e.g n = {} and type(n) will give; ", type(n))
+
+print("When you declare a variable and assign it to the method dict() like\
+ this m = dict(), it is assigned the type dict and type(m) will give; ", type(m))
 print('#' * 79)
-print("The key (sorrounded by square brackets, []) in a dict can be used to access a value, change a value or add a new object to a dict as follows:")
-print("we can print out the value of the brand in our list above like this thisdict['brand'] to get", thisdict['brand'])
+
+print("The key (sorrounded by square brackets, []) in a dict can be used to access\
+ a value, change a value or add a new object to a dict as follows:")
+
+print("we can print out the value of the brand in our list above like this\
+ thisdict['brand'] to get", thisdict['brand'])
 print('#' * 79)
+
 thisdict['brand']='mercedes'
-print("we can change the value of the brand in our list above like this thisdict['brand']='mercedes' and printing out the value, we get: {}".format(thisdict))
+print("we can change the value of the brand in our list above like this\
+ thisdict['brand']='mercedes' and printing out the value, we get: {}".format(thisdict))
 print('#' * 79)
+
 thisdict['make']='c200'
-print("we can add a new object to our list above like this thisdict['make']='c200' and printing out the dict we see our new object: {}".format(thisdict))
+print("we can add a new object to our list above like this thisdict['make']='c200'\
+ and printing out the dict we see our new object: {}".format(thisdict))
 print('#' * 79)
+
 print("There are a few methods that are useful for operating on dicts:")
-print("The get() method looks up to see if a key exists in the dict and if it does, returns the value of that key. If it does not exist, it returns the keyword None (by default) or another user defined response: e.g")
+print("The get() method looks up to see if a key exists in the dict and if it does,\
+ returns the value of that key. If it does not exist, it returns the keyword None\
+ (by default) or another user defined response: e.g")
 print('#' * 79)
+
 cc = thisdict.get('cc', 'The carrying capacity does not exist')
-print("We can look for a key 'cc' in our existing dict and if it does not exist print a message 'The carrying capacity does not exist' like this cc = thisdict.get('cc', 'The carrying capacity does not exist') and printing out the variable cc 'print(cc)' that stores the return value we get the output:",cc)
+print("We can look for a key 'cc' in our existing dict and if it does not exist\
+ print a message 'The carrying capacity does not exist' like this\
+ cc = thisdict.get('cc', 'The carrying capacity does not exist') and printing\
+ out the variable cc 'print(cc)' that stores the return value we get the output:",cc)
 print('#' * 79)
-print("The get() method is a better way to acces a value from a key in a dict compared to using just the square brackets and the key value like; thisdict['cc'] which returns an error if the key does not exist. Errors cause programs to break which is bad, so if you anticipate that a key might not exist, it is better to use the get() method which returns None or a custom user defined value.")
+
+print("The get() method is a better way to acces a value from a key in a dict\
+ compared to using just the square brackets and the key value like; thisdict['cc']\
+ which returns an error if the key does not exist. Errors cause programs to break\
+ which is bad, so if you anticipate that a key might not exist, it is better to use\
+ the get() method which returns None or a custom user defined value.")
 print('#' * 79)
+
 print("The dict data structure supports the membership operators 'in' and 'not in' which return a bool value of whether a key exists in the dict or not")
 print("You can also check if a value exists (or not) in a dictionary using the membership operators and calling the values() method on the dictionary value e.g 'Ford' in thisdict.values() same for the keys - 'year' in thisdict.keys()")
 print("'year' in thisdict returns {} and 'cc' not in thisdict returns {}".format('year' in thisdict, 'cc' not in thisdict))
