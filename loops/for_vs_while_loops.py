4e9a03af6b36286325df0e33571179c0738f1fdc
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-09 19:24:04 +0300

    for vs while use cases

--- loops/for_vs_while_loops.py
+++ loops/for_vs_while_loops.py
@@ -1,14 +1,25 @@
 # how to determine when to use for loops or while loops. Are the number of iterations known beforehand or unknown?
 print("for and while loops have different use cases")
+
 print("for loops are ideal when the number of iterations is known or finite  e.g")
-print("1. when you have an iterable collection e.g a list, set, tuple, string, dictionary e.g for object in some_collection:")
-print("2. When you are repeating a block of code a finite number of times using range() function e.g for i in range(5):")
+
+print("1. when you have an iterable collection e.g a list, set, tuple, string,\
+ dictionary i.e for object in some_collection:")
+
+print("2. When you are repeating a block of code a finite number of times using\
+ range() function e.g for i in range(5):")
 print('#' * 79)
+
 print("while loop is used when you want to iterate until a condition is met e.g")
+
 print("1. when you want to use a comparison operator e.g while i <= 10:")
+
 print("2. when you want to iterate based on a user's input e.g while user_input == 'y':")
-print("3. A while loop can also be used to create an infinite (never ending) loop using the condition while True:")
+
+print("3. A while loop can also be used to create an infinite (never ending) loop\
+ using the condition while True:")
 print('#' * 99)
+
 #PRACTICE
 # Question: What type of loop should we use?
 # You need to write a loop that takes the numbers in a given list named num_list:
