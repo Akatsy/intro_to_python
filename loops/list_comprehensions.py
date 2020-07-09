7f95f5e3aa6233d66bf72052f2400aaff002965f
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-09 19:51:32 +0300

    list comprehension syntax

--- loops/list_comprehensions.py
+++ loops/list_comprehensions.py
@@ -39,17 +39,20 @@ print('#' * 99)
 
 squares = [num**2 for num in range(21)]
 
-print("Suppose we want to create a list of all squares of numbers from 0 to 20, we could use list comprehensions as follows:\nsquares = [num**2 for num in range(21)]\nprint(squares)")
+print("Suppose we want to create a list of all squares of numbers from 0 to 20,\
+ we could use list comprehensions as follows:\nsquares = [num**2 for num in range(21)]\nprint(squares)")
 print(squares)
 print('#' * 99)
 
 print("We can add conditional statements to the list comprehensions as follows:")
-print("Suppose we want to print out just the squares of even numbers. We could add a conditional as follows:\neven_squares = [num**2 for num in range(21) if x % 2 ==0]")
+print("Suppose we want to print out just the squares of even numbers. We could add\
+ a conditional as follows:\neven_squares = [num**2 for num in range(21) if x % 2 ==0]")
 even_squares = [num**2 for num in range(21) if num % 2 == 0]
 print(even_squares)
 print('#' * 99)
 
-print("If you want to add an else statement, you have to move the conditional to the beginning of the list comprehension just after the expression to be evaluated for each element")
+print("If you want to add an else statement, you have to move the conditional to\
+ the beginning of the list comprehension just after the expression to be evaluated for each element")
 print("for example, say we wanted if the number is not even to cube it instead")
 
 weird_squares = [num**2 if num % 2 == 0 else num**3 for num in range(21)] 
@@ -58,7 +61,8 @@ print('#' * 99)
 #practice
 
 # Quiz: Extract First Names
-# Use a list comprehension to create a new list first_names containing just the first names in names in lowercase.
+# Use a list comprehension to create a new list first_names containing just the
+# first names in names in lowercase.
 
 names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
 
@@ -72,7 +76,8 @@ print(multiples_3)
 print('#' * 99)
 
 # Quiz: Filter Names by Scores
-# Use a list comprehension to create a list of names passed that only include those that scored at least 65.
+# Use a list comprehension to create a list of names passed that only include those
+# that scored at least 65.
 
 scores = {
              "Rick Sanchez": 70,
