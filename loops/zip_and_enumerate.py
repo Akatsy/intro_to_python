ce8597c6e2091f9046457f19bdb994c7ccb04e49
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-09 19:39:07 +0300

    unzip an iterable using zip

--- loops/zip_and_enumerate.py
+++ loops/zip_and_enumerate.py
@@ -23,22 +23,35 @@ print('#' * 99)
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f']
 numbers = [1, 2, 3, 4, 5, 6]
-print("for example if you had two lists as follows:\nletters = ['a', 'b', 'c', 'd', 'e', 'f'] and \nnumbers = [1, 2, 3, 4, 5, 6] \nand you called the zip function on them and then passed the result to the list function as follows list(zip(letters, numbers)) you would get:\n {}".format(list(zip(letters, numbers))))
+print("for example if you had two lists as follows:\nletters = ['a', 'b', 'c', 'd', 'e', 'f']\
+ and \nnumbers = [1, 2, 3, 4, 5, 6] \nand you called the zip function on them and then passed\
+ the result to the list function as follows list(zip(letters, numbers)) you would get:\
+ {}".format(list(zip(letters, numbers))))
 print('#' * 99)
-print("Note that zip returns an iterator object similar to the range() function so you have to call the list function on the result of zip to convert it to a list and see the results")
-print("You could also just iterate through using a for loop if you want to print out the result as follows\n {} to get the result below".format("for object in zip(letters, numbers):\n\tprint(object[0], object[1])"))
+
+print("Note that zip returns an iterator object similar to the range() function\
+ so you have to call the list function on the result of zip to convert it to a\
+ list and see the results")
+
+print("You could also just iterate through using a for loop if you want to print\
+ out the result as follows\n {} to get the result below".format("for object in zip(letters, numbers):\
+\n\tprint(object[0], object[1])"))
 for object in zip(letters, numbers):
     print(object[0], object[1])
 
 print('#' * 99)
-print("You can also unpack each tuple (tuple-unpacking or multiple variable assignment) and print the elements out as follows:\n{}".format("for letter, number in zip(letters, numbers):\n\tprint(letter, number)"))
+print("You can also unpack each tuple (tuple-unpacking or multiple variable assignment)\
+ and print the elements out as follows:\n{}".format("for letter, number in zip(letters, numbers):\
+\n\tprint(letter, number)"))
 for letter, number in zip(letters, numbers):
     print(letter, number)
 
 print('#' * 99)
 manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
 print("The zip function can also be used to unzip a list into tuples")
-print("for example, if given the following list;\n manifest = [(\"bananas\", 15), (\"mattresses\", 24), (\"dog kennels\", 42), (\"machine\", 120), (\"cheeses\", 5)]\n, we can use zip to unzip the list into tuples using\n items, weights = zip(*manifest)\n to get the result below")
+print("for example, if given the following list;\n manifest = [(\"bananas\", 15),\
+ (\"mattresses\", 24), (\"dog kennels\", 42), (\"machine\", 120), (\"cheeses\", 5)]\n,\
+ we can use zip to unzip the list into tuples using\n items, weights = zip(*manifest)\n to get the result below")
 items, weights = zip(*manifest)
 print(items, weights)
 
