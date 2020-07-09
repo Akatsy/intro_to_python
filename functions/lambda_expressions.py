9b3566183a5c7bcfc13e2b2207f0990be3c4eace
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-09 20:30:03 +0300

    lambda expressions definition and declaration

--- functions/lambda_expressions.py
+++ functions/lambda_expressions.py
@@ -1,15 +1,28 @@
-print("lambda expressions can be used to create anonymous functions i.e functions without a name")
-print("They are helpful when we want to create quick functions that are not needed later in your code")
-print("This can be especially useful in higher order functions (i.e functions that take other functions as arguments)")
+print("lambda expressions can be used to create anonymous functions i.e functions\
+ without a name")
+
+print("They are helpful when we want to create quick functions that are not needed\
+ later in your code")
+
+print("This can be especially useful in higher order functions (i.e functions that\
+ take other functions as arguments)")
 print('#' * 99)
 
 print("A lambda expression has the following parts:")
+
 print("1. the lambda keyword which signifies that this is a lambda expression")
-print("2. Following the lambda keyword is one or more arguments separated by a comma and then a colon(:). This will be passed as inputs to the function. (Their naming is just arbitrary)")
-print("3. Finally, after the colon is a statement that the function is to evaluate to get an output value, similar to what we would find in a return statement in a named function ")
-print("Given this structure, lambda expressions are not ideal for complex functions but can be very useful for simple functionss")
+
+print("2. Following the lambda keyword is one or more arguments separated by a comma\
+ and then a colon(:). This will be passed as inputs to the function. (Their naming is just arbitrary)")
+
+print("3. Finally, after the colon is a statement that the function is to evaluate\
+ to get an output value, similar to what we would find in a return statement in a named function ")
+
+print("Given this structure, lambda expressions are not ideal for complex functions\
+ but can be very useful for simple functions")
 print('#' * 99)
-print("for example the following function\n {} can be converted to a lambda function as follows:\n {}".format("def cubed(num):\n\treturn num ** 3\n\n", "\ncubed = lambda num: num**3"))
+print("for example the following function\n {} can be converted to a lambda function\
+ as follows:\n {}".format("def cubed(num):\n\treturn num ** 3\n\n", "\ncubed = lambda num: num**3"))
 
 def cubed(num):
     return num ** 3
