62d998381cd91e668b86794338353eb2775f7190
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-12 20:01:02 +0300

    creating a regex object

--- regexes/intro.py
+++ regexes/intro.py
@@ -8,21 +8,37 @@ print("For example, \d in a regex stands for a digit character i.e any single nu
  from 0-9 will match this regex")
 print("#" * 99)
 
-print("A regex defined as \d\d\d-\d\d\d-\d\d\d\d would tell Python to match a sequence of characters with 3 digits then a hyphen followed by 3 digits, another hyphen followed by 4 digits. Any text that does not match this search pattern would not be returned")
-print("Regexes can be much more complicated than that e.g adding an integer in braces after a regex like \d{3} is like saying match this pattern 3 times, so the above regex shortened would be \d{3}-\d{3}-\d{4}")
+print("A regex defined as \d\d\d-\d\d\d-\d\d\d\d would tell Python to match a sequence of\
+ characters with 3 digits then a hyphen followed by 3 digits, another hyphen followed by 4 digits.\
+ Any text that does not match this search pattern would not be returned")
+
+print("Regexes can be much more complicated than that e.g adding an integer in braces after a regex\
+ like \d{3} is like saying match this pattern 3 times, so the above regex shortened would be\
+ \d{3}-\d{3}-\d{4}")
 
 print("#" * 99)
 
 print("CREATING REGEX OBJECTS".center(66, '*'))
-print("All the regex functions in Python are in the re module, so we must always import the module if we want to use regexes: >>>import re")
+
+print("All the regex functions in Python are in the re module, so we must always import the module\
+ if we want to use regexes: >>>import re")
+
 import re
-print("Passing a string representing your regex (description of a pattern of text) to re.compile() function returns a Regular Expression pattern object or simply a regex object. ALWAYS REMEMBER TO USE A RAW STRING FOR THE REGEX")
-print("For example: phone_num_regex = re.compile(r'\d{3}-\d{3}-\d{4}') will return a regex object (to match a sequence of characters with 3 digits then a hyphen followed by 3 digits, another hyphen followed by 4 digits) and store it to the variable phone_num_regex")
+
+print("Passing a string representing your regex (description of a pattern of text) to re.compile()\
+ function returns a Regular Expression pattern object or simply a regex object. ALWAYS REMEMBER\
+ TO USE A RAW STRING FOR THE REGEX")
+
+print("For example: phone_num_regex = re.compile(r'\d{3}-\d{3}-\d{4}') will return a regex object\
+ (to match a sequence of characters with 3 digits then a hyphen followed by 3 digits, another\
+ hyphen followed by 4 digits) and store it to the variable phone_num_regex")
+
 phone_num_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
 
 print("#" * 99)
 
 print("MATCHING REGEX OBJECTS".center(66, '*'))
+
 print("A regex object's search method searches the string it is passed for a pattern matching the regex")
 print("search() will return the null value None if no match is found in the string")
 print("If a match is found (i.e a pattern matching the regex) then search method returns a Match Object which have a group() method that will return the actual matched text from the search string")
