38e55d3c5de47211da1d1d9385a93cae9533898a
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-07 16:49:34 +0300

    object and class definition

--- oop/intro.py
+++ oop/intro.py
@@ -1,3 +1,6 @@
 print(r'''
+Objects are a way of organizing code in a program and breaking things down to 
+make it easier to think about complex ideas
 
+Classes are a way of classifying things (objects) into groups and classes are what defines objects
 ''')
