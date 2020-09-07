398b806bff8264df1d231dfc18fb0e99bfb3b9e2
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-07 16:58:41 +0300

    sub-classes

--- oop/intro.py
+++ oop/intro.py
@@ -8,4 +8,15 @@ We define classes using the class keyword followed by name (convention is first 
 e.g
 class Things:
     pass
+
+If a class is a part of another class, then it is called a child of that class and the other class is
+its parent.
+To tell Python that a class is part of another class, we put the name of the parent class in parantheses
+after the name of our class
+e.g
+class Inanimate(Things):
+    pass
+
+class Mammals(Animals):
+    pass
 ''')
