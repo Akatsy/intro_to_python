9e294fe7446b12f9b5838ad2bb46ea7cd38cbadd
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-07 16:54:51 +0300

    how to define a class

--- oop/intro.py
+++ oop/intro.py
@@ -3,4 +3,9 @@ Objects are a way of organizing code in a program and breaking things down to
 make it easier to think about complex ideas
 
 Classes are a way of classifying things (objects) into groups and classes are what defines objects
+
+We define classes using the class keyword followed by name (convention is first letter is capitalized)
+e.g
+class Things:
+    pass
 ''')
