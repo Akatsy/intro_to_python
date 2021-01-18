4cf27b68fc2d680650478c5120f77a76564c3c40
mutwiri-2 <devmutwiri2@gmail.com>
2021-01-18 11:36:22 +0300

    limit line to 79 characters

--- scripting/importing_local_scripts/demo.py
+++ scripting/importing_local_scripts/demo.py
@@ -7,7 +7,8 @@ try:
 except Exception as e:
     print(e)
 finally:
-    print("The above is to demonstrate you cannot access an object from another script directly using just it's identifier")
+    print("The above is to demonstrate you cannot access an object from another\
+     script directly using just it's identifier")
 
 print(other_script.greeting)
 
